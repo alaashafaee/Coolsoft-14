11a9ce99dbe95dd06fb497338875eda94aa247bc
ahmed93 <hero.20112012@gmail.com>
2014-03-21 20:41:15 +0200

    fixing bugs

--- Framework_Tasks/C2_Ruby_On_Rails/app/controllers/users_controller.rb
+++ Framework_Tasks/C2_Ruby_On_Rails/app/controllers/users_controller.rb
@@ -13,15 +13,18 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 	end
+	
+	def edit
+		@user = User.find(params[:id])
+	end
 
 	def update
 		@user = User.new(params[:id])
-
 		respond_to do
 			if @user.update_attributes(params[:user])
         		redirect_to @user
       		else
-				render action: "edit"
+				render "edit"
      		 end
 		end
 	end
