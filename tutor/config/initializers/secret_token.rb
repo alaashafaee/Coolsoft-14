9933354477f93a354eaf3b7115814e0d23a256fe
ahmed93 <hero.20112012@gmail.com>
2014-03-22 17:45:44 +0200

    creating Tutor Project directory

---
+++ tutor/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tutor::Application.config.secret_key_base = '88125658ee62ba0e3ddde23afc0fd71a5b00c10835ee4b0931c00374b6a83c8b1e7feb324ea7d7e838a9cdb942520fcd36b78bf5643ab06cde04910f2490d1d2'
