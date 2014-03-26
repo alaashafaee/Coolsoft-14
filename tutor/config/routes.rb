652e797615cbfaf6ab8cbf7ff330ff408f33cffe
Mimi <mimi@ubuntu.(none)>
2014-03-26 15:57:02 +0200

    Adding problems controller

--- tutor/config/routes.rb
+++ tutor/config/routes.rb
@@ -4,6 +4,7 @@ Tutor::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'site#index'
+  resources :problems
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
