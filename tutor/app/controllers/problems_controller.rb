753f7d4390ba4af580c0509a11121301ae2322cd
Rami Khalil <thesavagekoder@gmail.com>
2014-03-27 16:05:30 +0200

    Properly renamed problem_not_found view.

--- tutor/app/controllers/problems_controller.rb
+++ tutor/app/controllers/problems_controller.rb
@@ -4,12 +4,13 @@ class ProblemsController < ApplicationController
 
 	def show 
 		@problem = Problem.find_by_id(params[:id])
+
 		if @problem.nil?
-		render "problem_not_found"
+			render "problem_not_found"
 		else
-		@track = @problem.track
-		@topic = @track.topic
-		@course = @topic.course
+			@track = @problem.track
+			@topic = @track.topic
+			@course = @topic.course
 		end
 	end
 
