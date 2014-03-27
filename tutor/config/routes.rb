05d485bf0f4118d0843f1070914a65d2f30ab090
metawaa <metawaa@Metawaa.(none)>
2014-03-27 13:39:24 +0200

    Adding Course Controller

--- tutor/config/routes.rb
+++ tutor/config/routes.rb
@@ -1,4 +1,7 @@
 Tutor::Application.routes.draw do
+  get "courses/destroy"
+  get "courses/create"
+  get "courses/show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
