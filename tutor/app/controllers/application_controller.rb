edf86c7164349c10e2e687dc3f5843b311d4c6b0
Ahmed Akram <a.akram93@gmail.com>
2014-03-27 21:35:12 +0200

    authentications modification

--- tutor/app/controllers/application_controller.rb
+++ tutor/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   	# Prevent CSRF attacks by raising an exception.
   	# For APIs, you may want to use :null_session instead.
-  	
+  	before_action :authenticate!
   	protect_from_forgery with: :exception
 	before_action :update_sanitized_params, if: :devise_controller?
 
@@ -15,5 +15,15 @@ class ApplicationController < ActionController::Base
 			elsif "#{resource_name}" == "teaching_assistant"
 				devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :password_confirmation)}
 			end
+		end
+
+		def authenticate!
+			if signed_in? or params[:controller].include?"devise"
+				return true
+			else
+				flash[:notice] = "You're not logged in!"
+				redirect_to :root
+			end
+
 		end
 end
--- tutor/app/controllers/site_controller.rb
+++ tutor/app/controllers/site_controller.rb
@@ -1,9 +1,5 @@
 class SiteController < ApplicationController
-
-
-
+	skip_before_filter :authenticate!
 	def index
 	end
-	
-
 end
