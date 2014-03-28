e6b86eb4408b61ee5628b807d98ded84690eeb68
linsara <linsara1@gmail.com>
2014-03-28 23:27:08 +0200

    documentation fixes#122

--- tutor/app/models/test_case.rb
+++ tutor/app/models/test_case.rb
@@ -1,8 +1,7 @@
 class TestCase < ActiveRecord::Base
-	
 	#Validations
-        validates :input , :output , :presence => true 
-    	validates :input , :length => { :minimum => 1 }
+    validates :input , :output , :presence => true 
+    validates :input , :length => { :minimum => 1 }
 	validates :output, :length => { :minimum => 1 }
 	validates :input , :length => { :maximum => 25}
 	validates :output, :length => { :maximum => 25 }
