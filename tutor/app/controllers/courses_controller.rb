563d7aa949c6f3d70a27cb87a8a0e91f9bc71d0d
Ahmed <Ahmed@Ahmeds-MacBook-Pro-7.local>
2014-03-28 17:06:06 +0200

    Course Sign-Up (2.6) withno database insertion due to the authentication dependency.

--- tutor/app/controllers/courses_controller.rb
+++ tutor/app/controllers/courses_controller.rb
@@ -1,7 +1,29 @@
 class CoursesController < ApplicationController
-	def show
+	def sign_up
+		@status = params[:status]	
+		case params[:status]
+		#shouls start from "1" -> Uni	
+		 when nil
+		 	@courses = Course.select(:university).distinct
+		 	@status = "1"
+		 when "2"
+		 	@courses = Course.where("university= " + "\""+ params[:university] + "\"").select(:semester).distinct
 
-			@courses = Lecturer.find_by_name('Lecturer2').courses
-		end
+		 when "3"
+		 	@courses = Course.where("semester= " + params[:semester])
+		 	
+		 when "4"
+		 	@course = Course.find_by_code(params[:code])
+		 when "5"
+			@course = Course.find_by_code(params[:code])
+			@lecturer = Lecturer.find(params[:lecturer])
+		 		
+		 when "6"
+			@course = Course.find_by_code(params[:code])
+			@lecturer = Lecturer.find(params[:lecturer])
+			#Insert in the DB
+			#@student = Student.find_by_id(...)
+			#@student.courses << @course	
+		 end 			
 	end
-
+end
