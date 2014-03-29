b85ccf3c21faa7fad2a22d7e731d9cd480f99d9c
Ahmed <Ahmed@Ahmeds-MacBook-Pro-7.local>
2014-03-29 22:26:33 +0200

    Add documentaion and convetiins

--- tutor/app/controllers/courses_controller.rb
+++ tutor/app/controllers/courses_controller.rb
@@ -1,27 +1,27 @@
 class CoursesController < ApplicationController
-	def sign_up
+	
+  #Course Sign-Up (2.6)
+  #University > Semester > Course > Instructor > Sign-Up > Confirmation
+  #Authored by Ahmed Moataz
+  def sign_up
 		@status = params[:status]	
 		case params[:status]
-		#shouls start from "1" -> Uni	
 		 when nil
 		 	@courses = Course.select(:university).distinct
 		 	@status = "1"
 		 when "2"
 		 	@courses = Course.where("university= " + "\""+ params[:university] + "\"").select(:semester).distinct
-
 		 when "3"
-		 	@courses = Course.where("semester= " + params[:semester])
-		 	
+		 	@courses = Course.where("semester= " + params[:semester])	
 		 when "4"
 		 	@course = Course.find_by_code(params[:code])
 		 when "5"
 			@course = Course.find_by_code(params[:code])
 			@lecturer = Lecturer.find(params[:lecturer])
-		 		
 		 when "6"
 			@course = Course.find_by_code(params[:code])
 			@lecturer = Lecturer.find(params[:lecturer])
-			#Insert in the DB
+			#Insertion in the DB
 			#student = Student.find(current_student.id)
 			#student.courses << @course	
 		 end 			
