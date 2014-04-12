33ab8d5108a17d676529004ca78a6e990635b3df
Ahmed Akram <a.akram93@gmail.com>
2014-04-12 15:11:44 +0200

    testing .ignore v1

--- tutor/db/migrate/20140323104910_create_users.rb
+++ tutor/db/migrate/20140323104910_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :namesasds
+      t.string :name
       t.string :email
       t.boolean :verified_type
       t.string :password
