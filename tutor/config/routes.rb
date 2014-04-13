a76416e493bbfc94038f005b4fd56f4f4265d321
MussabElDash <mussab14@gmail.com>
2014-04-13 16:13:09 +0200

    Modifing files routes.rb , seeds.rb

--- tutor/config/routes.rb
+++ tutor/config/routes.rb
@@ -1,4 +1,7 @@
 Tutor::Application.routes.draw do
+  resources :tracks do
+    post 'getProblems' , on: :member
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
