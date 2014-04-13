754c3558134b420ca25eba4001ffe388b2f586c6
Ahmed Akram <a.akram93@gmail.com>
2014-04-13 17:09:53 +0200

    adding courses controller

--- tutor/config/routes.rb
+++ tutor/config/routes.rb
@@ -1,4 +1,5 @@
 Tutor::Application.routes.draw do
+  resources :courses
   resources :topics
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
