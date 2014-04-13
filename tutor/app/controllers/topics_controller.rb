0b9b3bb297d8fd1c9412800d84e37eff497e4ca2
Ahmed Akram <a.akram93@gmail.com>
2014-04-13 15:33:32 +0200

    adding actions code

--- tutor/app/controllers/topics_controller.rb
+++ tutor/app/controllers/topics_controller.rb
@@ -1,16 +1,40 @@
 class TopicsController < ApplicationController
-  def new
-  end
 
-  def destroy
-  end
+    def destroy
+    end
 
-  def create
-  end
+    def new
+        @course = Course.find(params[:course_id])
+    end
 
-  def index
-  end
+    def index
+        @course = Course.find(params[:id])
+        @topics = @course.topics.order("created_at desc")
+    end
 
-  def show
-  end
+    def create
+        @new_topic = Topic.new
+        @new_topic.title = topic_params[:title]
+        @new_topic.description = topic_params[:description]
+        bool = @new_topic.save
+        if bool == true 
+            flash[:notice] = "Succeeded"
+            @course = Course.find(topic_params[:course_id])
+            @course.topics << @new_topic
+            redirect_to :controller => 'topics', :action => 'index', :id => @course.id
+        else
+            flash[:notice] = "Failed"
+            redirect_to :back
+        end
+    end
+
+    def show
+        @topic = Topic.find_by_id(params[:id])
+    end
+
+    private
+        def topic_params
+            params.require(:topic).permit(:title,:description, :course_id)
+        end
+
 end
