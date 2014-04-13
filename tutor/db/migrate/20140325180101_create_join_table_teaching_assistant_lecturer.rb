aaf4de11e000d123bf1d5bf65737e421a91127ad
Ahmed Akram <a.akram93@gmail.com>
2014-04-13 09:45:15 +0200

    deleting migration files and modifying the .gitignore

--- tutor/db/migrate/20140325180101_create_join_table_teaching_assistant_lecturer.rb
+++
@@ -1,9 +0,0 @@
-class CreateJoinTableTeachingAssistantLecturer < ActiveRecord::Migration
-  def change
-    create_join_table :teaching_assistants, :lecturers do |t|
-      # t.index [:teaching_assistant_id, :lecturer_id]
-      # t.index [:lecturer_id, :teaching_assistant_id]
-    end
-    add_index(:lecturers_teaching_assistants, [:teaching_assistant_id, :lecturer_id], :unique => true, :name => "TALecturers")
-  end
-end
