9d46b3cf813f2aacd0307a1077c272e4e2028584
Khaled <khaled@Khaled-L.(none)>
2014-04-13 20:07:29 +0200

    Issue #130 Added user type: lecturer registeration

--- tutor/config/routes.rb
+++ tutor/config/routes.rb
@@ -1,4 +1,5 @@
 Tutor::Application.routes.draw do
+  devise_for :lecturers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- tutor/db/schema.rb
+++ tutor/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140326150549) do
+ActiveRecord::Schema.define(version: 20140413172833) do
 
   create_table "admins", force: true do |t|
     t.datetime "created_at"
@@ -98,7 +98,19 @@ ActiveRecord::Schema.define(version: 20140326150549) do
     t.string   "lecturer_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "lecturers", ["email"], name: "index_lecturers_on_email", unique: true
+  add_index "lecturers", ["reset_password_token"], name: "index_lecturers_on_reset_password_token", unique: true
 
   create_table "lecturers_teaching_assistants", id: false, force: true do |t|
     t.integer "teaching_assistant_id", null: false
