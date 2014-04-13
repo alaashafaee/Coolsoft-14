b524dfa7868b7d4b46c6a0a4fb6f1dbf669bfa60
Linsara <linsara1@gmail.com>
2014-04-13 16:21:57 +0200

    Added proper documentation

--- tutor/app/controllers/test_cases_controller.rb
+++ tutor/app/controllers/test_cases_controller.rb
@@ -1,13 +1,26 @@
 class TestCasesController < ApplicationController
+# [Add test case-story 4.8]
+# Shows all the test cases.
+# Parameters: None
+# Returns:List of all the test cases related to a certain problem.
+# Author: Lin 
 def index 
     @test_cases = TestCase.all
 end
-#Lin: Displays all test cases.
+# [Add test case-story 4.8]
+# Display the form that is used to add a test case. 
+# Parameters: None
+# Returns: 
+# Author: Lin 
 def new 
-    @test_case = TestCase.new(params[:id])  
+    @test_case = TestCase.new()  
 end
-#Lin: Saves the test case into the data base. 
-#     It is where the test case form will submit to. 
+# [Add test case-story 4.8]
+# Saves the new test case into the database.(What the form the 'new' method will submit to)  
+# Parameters: None
+# Returns: In case of success a flash notice will appear:"Post created successfully"
+#        In case of failure a flash notice will appear:"Can't add test case!"
+# Author: Lin
 def create
 @test_case = TestCase.new(post_params) 
     if @test_case.save
@@ -18,8 +31,11 @@ def create
         redirect_to :back
 	end 
 end
-
-#Lin: This private message is used to control the parameters that can be accessed.
+# [Add test case-story 4.8]
+# private method. Controls the test case parameters that can be accessed.
+# Parameters: None
+# Returns:
+# Author: Lin
 private
 def post_params
     params.require(:test_case).permit(:input, :output)
