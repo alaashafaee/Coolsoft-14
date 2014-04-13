b8f2d5ff07a154ffe3ad14cce720de4808a39118
Linsara <linsara1@gmail.com>
2014-04-13 12:08:37 +0200

    Fixed indentaion errors

--- tutor/app/models/test_case.rb
+++ tutor/app/models/test_case.rb
@@ -1,21 +1,13 @@
 class TestCase < ActiveRecord::Base
-	
 	#Validations
-        validates :input , :output , :presence => true 
-    	validates :input , :length => { :minimum => 1 }
+    validates :input , :output , :presence => true 
+    validates :input , :length => { :minimum => 1 }
 	validates :output, :length => { :minimum => 1 }
 	validates :input , :length => { :maximum => 25}
 	validates :output, :length => { :maximum => 25 }
-	
 	#Relations
 	belongs_to :model_answer
-
 	belongs_to :owner, class_name: 'Stuff', foreign_key: :stuff_id
-
-	
 	#Scoops
 	#Methods
-
-
-
 end
--- tutor/config/routes.rb
+++ tutor/config/routes.rb
@@ -1,12 +1,10 @@
 Tutor::Application.routes.draw do
-get 'courses/show'
-
-resources :test_cases
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root 'site#index'
+    get 'courses/show'
+    resources :test_cases
+    # The priority is based upon order of creation: first created -> highest priority.
+    # See how all your routes lay out with "rake routes".
+    # You can have the root of your site routed with "root"
+    root 'site#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
