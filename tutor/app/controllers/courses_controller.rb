a8ca0fa9f5a359b32a0269a93231fb582fd1786f
Rania Abdel Fattah <rania@rania-Satellite-L655.(none)>
2014-04-13 16:36:54 +0200

    create from existing

--- tutor/app/controllers/courses_controller.rb
+++ tutor/app/controllers/courses_controller.rb
@@ -1,4 +1,9 @@
 class CoursesController < ApplicationController
+	# [Create from Existant Course - Story 2.5]
+	# Return the course specified by the id or else return all the courses
+	# Parameters:
+	#	id: The course id
+	# Author: Rania Abdel Fattah 
 	def show
 		@courses = []
 		if(params[:id])
@@ -9,22 +14,29 @@ class CoursesController < ApplicationController
 		end
 
 	end
-
-	# This method get all the existing courses
+	# [Create from Existant Course - Story 2.5]
+	# Return all the existing courses
+	# Author: Rania Abdel Fattah
 	  def existing
 	  	@courses = Course.all  
 	  end
-
-	# This method duplicate the courses that it is found and save it in a new course
-	  def duplicate
+	# [Create from Existant Course - Story 2.5]
+	# Duplicate the courses that it is found and save them in a new coursees
+	# Parameters:
+	#	id: The course id
+	# Author: Rania Abdel Fattah
+	 def duplicate
 	  	permit
 	  	course = Course.find_by_id(params[:Course][:id])
   		newCourse = course.dup
   		newCourse.save
   		redirect_to newCourse
 	end
-
-	# This method choose with parameter should be listed
+	# [Create from Existant Course - Story 2.5]
+	# Choose which parameter should be listed
+	# Parameters:
+	#	id: The course id
+	# Author: Rania Abdel Fattah
 	def permit
 		params.require(:Course).permit(:id)
 	end
