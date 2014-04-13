22ee674e345917d9ee11c0d6dd5a4e4ab702506b
Rania Abdel Fattah <rania@rania-Satellite-L655.(none)>
2014-04-13 14:36:46 +0200

    create from existing

---
+++ tutor/app/controllers/courses_controller.rb
@@ -0,0 +1,31 @@
+class CoursesController < ApplicationController
+	def show
+		@courses = []
+		if(params[:id])
+			course = Course.find_by_id(params[:id])
+			@courses << course
+		else
+			@courses = Course.all
+		end
+
+	end
+
+	# This method get all the existing courses
+	  def existing
+	  	@courses = Course.all  
+	  end
+
+	# This method duplicate the courses that it is found and save it in a new course
+	  def duplicate
+	  	permit
+	  	course = Course.find_by_id(params[:Course][:id])
+  		newCourse = course.dup
+  		newCourse.save
+  		redirect_to newCourse
+	end
+
+	# This method choose with parameter should be listed
+	def permit
+		params.require(:Course).permit(:id)
+	end
+end
--- tutor/config/routes.rb
+++ tutor/config/routes.rb
@@ -1,4 +1,5 @@
 Tutor::Application.routes.draw do
+  get "courses/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
