54a6818649917f790520d6803a27abc0e91f8ef3
Rania Abdel Fattah <rania@rania-Satellite-L655.(none)>
2014-04-14 17:44:39 +0200

    Issue #163 create from existing

--- tutor/app/controllers/courses_controller.rb
+++ tutor/app/controllers/courses_controller.rb
@@ -11,20 +11,20 @@ class CoursesController < ApplicationController
 	#	id: The course id
 	# Author: Rania Abdel Fattah
 	def duplicate
-	  	course = Course.find_by_id(permitdup[:id])
-  		new_course = course.dup
-  		if new_course.save
-  			redirect_to :back
-  		else
-  			redirect_to :back
-  		end
+		course = Course.find_by_id(permitdup[:id])
+		new_course = course.dup
+		if new_course.save
+			redirect_to :back
+		else
+			redirect_to :back
+		end
 	end
 	# [Create from Existant Course - Story 2.5]
 	# Choose which parameter should be listed
 	# Parameters:
 	#	id: The course id
 	# Author: Rania Abdel Fattah
-	private 
+	private
 		def permitdup
 			params.require(:course).permit(:id)
 		end
