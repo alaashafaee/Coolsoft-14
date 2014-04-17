75f61d550efd70a1238a0097ca954ee2ee626d38
AmirGeorge <dangerousmiro@gmail.com>
2014-04-18 01:47:47 +0200

    added whenever gem, changed action name to reminder_email

--- tutor/app/mailers/system_reminders.rb
+++ tutor/app/mailers/system_reminders.rb
@@ -1,6 +1,6 @@
 class SystemReminders < ActionMailer::Base
   default from: "from@example.com"
-  def welcome_email(current_student)
+  def reminder_email(current_student)
     @user = current_student
     @url  = 'http://localhost:3000'
     mail(to: @user.email, subject: 'We miss you at our Awesome Tutors Website')
