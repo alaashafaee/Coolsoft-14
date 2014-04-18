aa113a3da19b638890ad7f274d5b91857fca3c9d
Rami Khalil <thesavagekoder@gmail.com>
2014-04-18 16:32:09 +0200

    Indentation issues.

--- tutor/app/models/debugger.rb
+++ tutor/app/models/debugger.rb
@@ -11,8 +11,7 @@ class Debugger < ActiveRecord::Base
 	# [Execulte Line By Line - 3.8]
 	# Converts the list of execution states to JSON
 	# Parameters:
-	# 	states_table: The hash table containing the states of 
-	# 						execution of a program
+	# 	states_table: The hash table containing the states of execution of a program
 	# Returns: JSON representation of execution table
 	# Author: Rami Khalil
 	def execution_states_json(states_table)
