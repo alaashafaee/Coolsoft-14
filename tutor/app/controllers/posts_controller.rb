ca557ea8c23522f708a18576cbf54ce0032db16f
Ahmed Atef <Ahmed@Ahmeds-MacBook-Pro-8.local>
2014-04-21 01:27:13 +0200

    Adding documentation for my post controller

--- tutor/app/controllers/posts_controller.rb
+++ tutor/app/controllers/posts_controller.rb
@@ -1,11 +1,28 @@
 class PostsController < ApplicationController
 
-
+	# [Add Post - Story 1.13]
+	# Description: This action takes the passed discussion board id and assigns
+	#              the discussion board with that id to an instance variable.
+	# Parameters:
+	#	params[:discussion_board_id]: The discussion board id
+	# Returns: 
+	# 	none
+	# Author: Ahmed Atef
 	def new
 		@discussion_board = DiscussionBoard.find_by_id(params[:discussion_board_id])
 		@new_post = Post.new
 	end
-
+	# [Add Post - Story 1.13]
+	# Description: This action takes the passed parameters from 
+	#              the add post form, creates a new Post record
+	#              and assigns it to the respective discussion board.If the 
+	#              creation fails the user is redirected to the form
+	# Parameters:
+	#	topic_params[]: A list that has all fields entered by the user to in the
+	# 					add_post_form
+	# Returns: 
+	# 	flash[:notice]: A message indicating the success or failure of the creation
+	# Author: Ahmed Atef
 	def create
 	@new_post = Post.new(post_params)
 	#@new_post.content = post_params[:content]
@@ -26,7 +43,6 @@ class PostsController < ApplicationController
 			flash[:notice] = "Post successfully created"
 			@discussion_board = DiscussionBoard.find(discussion_board_params[:discussion_board_id])
 			@discussion_board.posts << @new_post
-	    #		redirect_to :action => 'index'
 		else
 			if @new_post.errors.any?
 				flash[:notice] = @new_post.errors.full_messages.first
@@ -34,6 +50,14 @@ class PostsController < ApplicationController
 			render :action => 'new'  
 		end
 	end
+	# [Add Post - story 1.13]
+	# private method. Controls the post form parameters that can be accessed.
+	# Parameters: 
+	# 	content: The content of the Post
+	# 	title: The title of the Post
+	# 	discussion_board_id: hidden field for the discussion board id
+	# Returns: None
+	# Author: Ahmed Atef
 		private
 		def post_params 
 			params.require(:post).permit(:content,:title)
