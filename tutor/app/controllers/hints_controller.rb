a423e4ae80f3788772ff0aeb06a8025a2a24d7d3
Mohamed Fadel <mohamedfadel@Fadel.(none)>
2014-04-21 00:23:36 +0200

    Issue #210 Adding helping hints

--- tutor/app/controllers/hints_controller.rb
+++ tutor/app/controllers/hints_controller.rb
@@ -10,4 +10,34 @@ class HintsController < ApplicationController
 		@hints = Hint.all
 		@hints_check=Hint.all
 	end
+		# [Add Problem - 4.4]
+	# Creates a new record to Problem Table
+	# Parameters:
+	#   title: problem's title through permitCreate action
+	#   description: problem's description through permitCreate action
+	# Returns: Redirects to edit page on success, refreshes on failure
+	# Author: Abdullrahman Elhusseny
+	def create
+		p = Hint.new(permitCreate)
+
+		if p.save
+			redirect_to :action => "new", :id => p.id
+		else 
+			flash.keep[:notice] = "Hint is missing paramaters"
+			redirect_to :back
+		end
+	end
+
+	# [Add Problem - 4.4]
+	# Passes the input of the form as paramaters for create action to use it
+	# Parameters:
+	#   title: problem's title
+	#   description: problem's description
+	# Returns: params to create action
+	# Author: Abdullrahman Elhusseny
+	def permitCreate
+		params.require(:Hint).permit(:title , :category, :submission_counter, :message)
+	end
+	def create_new
+	end	
 end
