0d609758697aa2eda8b259960d59c2a4f1673023
Rami Khalil <thesavagekoder@gmail.com>
2014-04-20 19:48:45 +0200

    Removed unused method from debugger.rb

--- tutor/app/models/debugger.rb
+++ tutor/app/models/debugger.rb
@@ -8,14 +8,4 @@ class Debugger < ActiveRecord::Base
 	
 	#Methods
 
-	# [Execute Line By Line - 3.8]
-	# Converts the list of execution states to JSON
-	# Parameters:
-	# 	states_table: The hash table containing the states of execution of a program
-	# Returns: JSON representation of execution table
-	# Author: Rami Khalil
-	def execution_states_json(states_table)
-		
-	end
-
 end
