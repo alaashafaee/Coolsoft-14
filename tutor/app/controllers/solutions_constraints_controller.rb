b6506c3c44842a315e65d34f52de7e193da1b9bc
Rania Abdel Fattah <rania@rania-Satellite-L655.(none)>
2014-04-20 16:11:32 +0200

    Issue #255 edit solutions constraints

--- tutor/app/controllers/solutions_constraints_controller.rb
+++ tutor/app/controllers/solutions_constraints_controller.rb
@@ -1,21 +1,30 @@
 class SolutionsConstraintsController < ApplicationController
+	def index
+		@parameters = MethodParameter.all
+		@variables = VariableConstraint.all 
+		#@methods = MethodConstraints.all 
+	end
+
 	def show
 		@parameters = MethodParameter.find_by_id(params[:id])
 		@variables = VariableConstraint.find_by_id(params[:id])
-		#if @constraints.nil?
-		#	render "constraints_not_Found"
-		#end
+		#@methods = MethodConstraints.find_by_id(params[:id])
+		if @parameters.nil?
+			redirect_to :action => 'index'
+		end
 	end
 
 	def edit
 		if lecturer_signed_in? || teaching_assistant_signed_in?
 			@parameters = MethodParameter.find_by_id(params[:id])
-			@variables = VariableConstraint.find_by_id(params[:id])	
+			@variables = VariableConstraint.find_by_id(params[:id])
+			#@methods = MethodConstraints.find_by_id(params[:id])	
 		end
 	end
 
 	#def delete
 	#	constraint.find(params[id]).destroy
+	#	flash[:success_deletion] = "Constraints deleted."
 	#end
 
 end
