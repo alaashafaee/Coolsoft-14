e629c815f2802ed311bf8120cdc26c87d1462e01
Mimi <mimi@ubuntu.(none)>
2014-04-21 23:45:23 +0200

    addin hints controller and its view

---
+++ tutor/app/controllers/hints_controller.rb
@@ -0,0 +1,45 @@
+class HintsController < ApplicationController
+	#t.text     "message"
+    #t.boolean  "category"
+    #t.integer  "submission_counter"
+    #t.integer  "model_answer_id"
+    #t.integer  "owner_id"
+    #t.string   "owner_type"
+
+	
+    def show
+		@new_Tip = Problem.find_by_id(params[:id])
+	end
+   
+    # [Edit helping hints - Story 4.13 ]
+	# Description: This action takes the passed parameters from 
+	#              the creation form, creates a new Hint record
+	#              and assigns it to the respective problem. If the 
+	#              creation fails the user is redirected to the form
+	#              with a "Failed" message.
+	# Parameters:
+	#	hint_params[]: A list that has all fields entered by the user to in the
+	# 					create_topic_form
+	# Returns: 
+	# 	flash[:notice]: A message indicating the success or failure of the creation
+	# Author: Mimi
+	def create
+		@new_Tip = Hint.new(hint_params)
+		bool = @new_Tip.save
+		if bool == true 
+			flash[:notice] = "Hint successfully created"
+			#redirect_to :action => 'create'
+			redirect_to :back
+		else
+			if @new_Tip.errors.any?
+				flash[:notice] = @new_Tip.errors.full_messages.first
+			end
+			redirect_to :back
+		end
+	end	
+
+	private
+		def hint_params
+			params.require(:hint).permit(:message,:submission_counter)
+		end
+end
--- tutor/config/routes.rb
+++ tutor/config/routes.rb
@@ -29,6 +29,7 @@ Tutor::Application.routes.draw do
 	resources :solutions
 	resources :problems
 	resources :topics
+	resources :hints
 
 	# Example resource route with options:
 	#   resources :products do
