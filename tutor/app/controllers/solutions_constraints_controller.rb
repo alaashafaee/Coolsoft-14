b4c2c5778940478d04eed318f3abfb88b31a778d
Rania Abdel Fattah <rania@rania-Satellite-L655.(none)>
2014-04-21 21:58:22 +0200

    Issue #255 solutions constraints documentation

--- tutor/app/controllers/solutions_constraints_controller.rb
+++ tutor/app/controllers/solutions_constraints_controller.rb
@@ -1,10 +1,25 @@
 class SolutionsConstraintsController < ApplicationController
+	# [Edit Solutions' Constraints - Story 4.14]
+	# Display all the parameters and the names of the method and the variables
+	# Parameters: 
+	#   Parameter: null
+	# Returns: 
+	# 	@parameters: A list of all the methods parameters
+	# 	@variables: A list of all the variables constraints
+	# 	@methods: A list of all the methods constraints
+	# Author: Rania Abdel Fattah
 	def index
 		@parameters = MethodParameter.all
 		@variables = VariableConstraint.all 
 		@methods = MethodConstraint.all 
 	end
 
+	# [Edit Solutions' Constraints - Story 4.14]
+	# Display only one record
+	# Parameters: 
+	#   params[:id]: The method parameter and the variable constraint and the method variable
+	# Returns: null
+	# Author: Rania Abdel Fattah
 	def show
 		@parameters = MethodParameter.find_by_id(params[:id])
 		@variables = VariableConstraint.find_by_id(params[:id])
@@ -14,6 +29,12 @@ class SolutionsConstraintsController < ApplicationController
 		end
 	end
 
+	# [Edit Solutions' Constraints - Story 4.14]
+	# Brief description of method
+	# Parameters: 
+	#   Parameter: description
+	# Returns: in case of success or failure
+	# Author: Your Name
 	def edit
 		if lecturer_signed_in? || teaching_assistant_signed_in?
 			@parameters = MethodParameter.find_by_id(params[:id])
@@ -22,6 +43,12 @@ class SolutionsConstraintsController < ApplicationController
 		end
 	end
 
+	# [Edit Solutions' Constraints - Story 4.14]
+	# Brief description of method
+	# Parameters: 
+	#   Parameter: description
+	# Returns: in case of success or failure
+	# Author: Your Name
 	def update
 		if @parameters = MethodParameter.find_by_id(params[:id]) &&
 			@variables = VariableConstraint.find_by_id(params[:id]) &&
@@ -48,6 +75,12 @@ class SolutionsConstraintsController < ApplicationController
 		end
 	end
 
+	# [Edit Solutions' Constraints - Story 4.14]
+	# Brief description of method
+	# Parameters: 
+	#   Parameter: description
+	# Returns: in case of success or failure
+	# Author: Your Name
 	private
 	def constraint_params
 		params.require(:parameter).permit(:parameters, :variables, :methods)
