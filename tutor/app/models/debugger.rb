c5ae313f414538d7bd3f9c856a229e4f3b9c463c
Khaled <khaled@Khaled-L.(none)>
2014-04-21 17:13:39 +0200

    Issue #193 Added method for getting values of objects

--- tutor/app/models/debugger.rb
+++ tutor/app/models/debugger.rb
@@ -12,7 +12,7 @@ class Debugger < ActiveRecord::Base
 	def get_variables
 		method_arguments = []
 		local_variables = []
-		instance_variables = []
+		instance_variables = get_instance_variables
 		flag = 0
 		input "locals"
 		output_buffer = buffer_until_complete
@@ -26,10 +26,27 @@ class Debugger < ActiveRecord::Base
 			end
 			variable_value = get_value line
 			if flag == 1
-				method_arguments << variable_value
+				method_arguments = method_arguments + variable_value
 			else
-				local_variables << variable_value
+				local_variables = local_variables + variable_value
 		end
+		return method_arguments + local_variables + instance_variables
+	end
+
+	def get_value variable
+		result = []
+		if variable.match("instance")
+			variable_name = get_name variable
+			input "dump " + variable_name
+			output_buffer = buffer_until_complete
+			result << variable
+			input "print " + variable_name
+			output_buffer = buffer_until_complete
+			result << variable
+		else
+			result << variable
+		end
+		return result
 	end
 
 end
