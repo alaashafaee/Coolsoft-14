9aa8157e5ae3bc12f21e878484c52574f6bbd7ce
AmirGeorge <dangerousmiro@gmail.com>
2014-04-21 13:54:07 +0200

    removed system_reminders.rb belonging to the other branch

--- tutor/app/mailers/system_reminders.rb
+++
@@ -1,8 +0,0 @@
-class SystemReminders < ActionMailer::Base
-  default from: "from@example.com"
-  def welcome_email(current_student)
-    @user = current_student
-    @url  = 'http://localhost:3000'
-    mail(to: @user.email, subject: 'We miss you at our Awesome Tutors Website')
-  end
-end
