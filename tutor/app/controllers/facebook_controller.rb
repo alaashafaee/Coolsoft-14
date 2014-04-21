b9405b8efb360296ac5fec4562e5cf4264711e1c
AmirGeorge <dangerousmiro@gmail.com>
2014-04-21 15:37:27 +0200

    removed fbgraph code in facebook controller

--- tutor/app/controllers/facebook_controller.rb
+++ tutor/app/controllers/facebook_controller.rb
@@ -1,10 +1,4 @@
 class FacebookController < ApplicationController
-  def index
-  	me = FbGraph::Student.me(ACCESS_TOKEN)
-	me.feed!(
-  		:message => 'Updating via FbGraph',
-	)
-  end
   def show
   	render ('index')
   end
