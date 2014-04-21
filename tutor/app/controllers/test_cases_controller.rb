194ce3d3e1b422a39aa57e782129a6a934e4ebfa
Nadine Adel <nadine@nadine-Inspiron-N4050.(none)>
2014-04-21 21:56:54 +0200

    Edit view is craeted

--- tutor/app/controllers/test_cases_controller.rb
+++ tutor/app/controllers/test_cases_controller.rb
@@ -37,6 +37,26 @@ def edit
 	@test_case =TestCase.find(params[:id])
 
 end
+
+def update
+		 @test_case = TestCase.find(params[:id])
+      if @test_case.update_attributes(post_params)
+      	flash[:notice] = "Your Testcase is now updated"
+         redirect_to :action => 'new'
+      else
+         
+        flash[:notice] = "Your Answer is not updated"
+      end
+	#		<% answers = ModelAnswer.find(params[:id]) %>
+	#	<% answers.update(answer:'oppaa') %>
+	#	<% answers.save %
+	#	if params[:second_button]
+		
+	#else
+	#	flash[:notice] = "Your Answer can notddd be added "
+	#	end
+
+	end
 # [Add test case-story 4.8]
 # private method. Controls the test case parameters that can be accessed.
 # Parameters: None
