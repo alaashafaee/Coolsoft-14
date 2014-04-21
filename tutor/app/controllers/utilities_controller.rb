f68889e6c1fb8c10def05d23e56d97f735d76b3a
Ahmed ELAssuty <ahmed.elassuty214@gmail.com>
2014-04-21 16:53:58 +0200

    Issue #221 ajax feature

--- tutor/app/controllers/utilities_controller.rb
+++ tutor/app/controllers/utilities_controller.rb
@@ -7,17 +7,11 @@ class UtilitiesController < ApplicationController
 	# Author: Ahmed Elassuty
 	def simple_search
 		@lecturers = Lecturer.search(params[:search])
-		puts @lecturers.present?
 		@students = Student.search(params[:search])
-		puts "______________________________________"
-		puts @students.inspect
 		@teaching_assisstants = TeachingAssistant.search(params[:search])
-		puts @teaching_assisstants.present?
-		puts ">>>>>>>>>>>>>>>>>>>>>>>>>>"
 		@courses = Course.search(params[:search])
-		puts @courses.inspect
-		puts "_______________________________"
 		respond_to do |format|
+			format.js #render 'simple_search.js.erb'
 			format.html
 		end
 	end
