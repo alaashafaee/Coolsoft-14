4d51f4b9a7900c3511d2173b82f23de03b6c9efb
AmirGeorge <dangerousmiro@gmail.com>
2014-04-21 21:43:45 +0200

    removed unnecessary controller method

--- tutor/app/controllers/facebook_controller.rb
+++ tutor/app/controllers/facebook_controller.rb
@@ -1,10 +1,2 @@
 class FacebookController < ApplicationController
-# [Post to Facebook - Story 5.1]
-# Renders the view that prompts user to post to facebook
-# Parameters: None
-# Returns: None
-# Author: Amir George
-	def show
-		render ('index')
-	end
 end
