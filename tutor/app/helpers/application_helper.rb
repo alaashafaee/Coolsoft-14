01754af8ca45b83a6cb3ce5660926c104cf6a13c
Ahmed ELAssuty <ahmed.elassuty214@gmail.com>
2014-04-21 17:23:53 +0200

    Issue #221 clear code and fix design

--- tutor/app/helpers/application_helper.rb
+++ tutor/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	
+	# [Simple Search - Story 1.22]
+	# helper to handle ajax search
+	# Parameters: string controller
+	# Returns: boolean if the requested controller equals the passed one
+	# Author: Ahmed Elassuty
+	def controller?(controller)
+		params[:controller].include?controller
+	end
 end
