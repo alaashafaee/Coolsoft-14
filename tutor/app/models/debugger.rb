0f878f2a5c0cbdef44b5e7fa6d27f658155a9846
Khaled <khaled@Khaled-L.(none)>
2014-04-21 16:31:04 +0200

    Issue #193 Added method for getting variables

--- tutor/app/models/debugger.rb
+++ tutor/app/models/debugger.rb
@@ -1,3 +1,4 @@
+require "open3"
 class Debugger < ActiveRecord::Base
 	
 	#Validations
@@ -7,5 +8,28 @@ class Debugger < ActiveRecord::Base
 	#Scoops
 	
 	#Methods
+
+	def get_variables
+		method_arguments = []
+		local_variables = []
+		instance_variables = []
+		flag = 0
+		input "locals"
+		output_buffer = bufferUntilComplete
+		output_buffer.each_line do |line|
+			if line == "Method arguments:"
+				flag = 1
+				next
+			else
+				flag = 2
+				next
+			end
+			variable_value = get_value line
+			if flag == 1
+				method_arguments << variable_value
+			else
+				local_variables << variable_value
+		end
+	end
 
 end
