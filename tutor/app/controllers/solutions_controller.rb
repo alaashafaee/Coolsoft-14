621f24fad35011d23309fbf9bbde852f8c4eebd1
Mohamed Saeed <mohamedsaeed@mohamedsaeed-HP-Pavilion-dv6-Notebook-PC.(none)>
2014-04-22 23:38:57 +0200

    changing some of my logic

--- tutor/app/controllers/solutions_controller.rb
+++ tutor/app/controllers/solutions_controller.rb
@@ -7,16 +7,18 @@ class SolutionsController < ApplicationController
 	# Returns: none
 	# Author: MOHAMEDSAEED
 	def create
-		solution = Solution.new({problem_id: params[:problem_id], code: params[:code],
+		@solution = Solution.new({problem_id: params[:problem_id], code: params[:code],
 					time: params[:time]})
-		solution.student_id = current_student.id
-		solution.length = solution.code.length
-		testcases = solution.problem.test_cases
-		solution.status = 0
+		@solution.student_id = current_student.id
+		@solution.length = @solution.code.length
+		testcases = @solution.problem.test_cases
+		@solution.status = 0
+		#@solution.save
+		#file = @solution.file_name
 		#response_message = Solution.validate(solution.code , testcases , file)
 		response_message = {:success => ["Success"], :failure => ["Failure"]}
 		#solution.status = response_message[:status]
-		solution.save
+		#solution.save
 		render json: response_message
 	end
 	
