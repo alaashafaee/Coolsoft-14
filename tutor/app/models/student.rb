18941187e40f429e3920d1b667c8debcb7a4bd39
Mohab <mohab@mohab-Aspire-4750.(none)>
2014-04-22 20:26:17 +0200

    Issue #200 displaying recommended problems

--- tutor/app/models/student.rb
+++ tutor/app/models/student.rb
@@ -80,11 +80,11 @@ class Student < ActiveRecord::Base
 		recommended_problems = Recommendation.where(:student_id => self.id)
 		recommended_problems_hash = Hash.new
 		recommended_problems.each do |problem|
-			recommended_problems_hash[problem.id] = Hash.new
-			recommender_name = Student.where(:id => problem.recommender_id).recommender_name
-			problem_title = Problem.where(:id => problem.id).title
-			recommended_problems_hash[problem.id]['recommender_name '] = recommender_name
-			recommended_problems_hash[problem.id]['ptoblem_title'] = problem.title
+			recommended_problems_hash[problem.problem_id] = Hash.new
+			recommender_name = Student.where(:id => problem.recommender_id).take.name
+			problem_title = Problem.where(:id => problem.problem_id).take.title
+			recommended_problems_hash[problem.problem_id]['recommender_name'] = recommender_name
+			recommended_problems_hash[problem.problem_id]['problem_title'] = problem_title
 		end
 		return recommended_problems_hash
 	end
