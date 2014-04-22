7d64846cc413811fceb8a026be116974f6177588
AmirGeorge <dangerousmiro@gmail.com>
2014-04-22 23:38:04 +0200

    added smtp configuraion for mail

--- tutor/config/environments/development.rb
+++ tutor/config/environments/development.rb
@@ -1,4 +1,5 @@
 Tutor::Application.configure do
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -13,9 +14,6 @@ Tutor::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -27,6 +25,19 @@ Tutor::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  # Configuring Devise mailer in Development environment
+  # Configuring mailing options in Development environment
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :user_name => "CoolSoft14@gmail.com",
+    :password => "coolsoftwillrockandwin",
+    :authentication => 'plain',
+    :enable_starttls_auto => true
+  }
+
 end
