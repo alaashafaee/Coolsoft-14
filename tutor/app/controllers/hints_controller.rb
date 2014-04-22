41cb87c873ea275ee177a48a6f1139ad649c3f6b
Mohamed Fadel <mohamedfadel@Fadel.(none)>
2014-04-22 13:11:45 +0200

    Issue #210 Adding problem hints

--- tutor/app/controllers/hints_controller.rb
+++ tutor/app/controllers/hints_controller.rb
@@ -1,20 +1,30 @@
 class HintsController < ApplicationController
-# [View hints and tips]
-# It fetches from database all the previous hints.
-# Parameters: 
-#   @hints: All the previous hints that had been entered before.
-#   @hints: All the previous hints that had been entered before to check if it is a hint or a tip.
-#   Return : none
-# Author: Nadine Adel
-	def new
+
+	@@answer_id = nil
+
+	# [View hints and tips-Story 4.22]
+	# It fetches from database all the previous hints.
+	# Parameters: 
+	#   @hints: All the previous hints that had been entered before.
+	#   @hints_check: All the previous hints that had been entered before to check if it is a hint or a tip.
+	# Return : table with all previous hints
+	# Author: Nadine Adel
+	def index
+
 		@hints = Hint.all
 		@hints_check=Hint.all
 	end
+	def new
+		@answer = ModelAnswer.find(params[:model_answer_id])
+		@@answer_id = params[:model_answer_id]
+	end
+	
 	# [Adding Helping Hints - 4.12]
 	# Creates a new record to Hint Table
 	# Parameters:
 	# 	owner_id: ID of the hint owner
 	# 	owner type: type of hint owner
+	# 	category: states whether it is a hint or tip
 	# Returns: Redirects to edit page on success, refreshes on failure
 	# Author: Mohamed Fadel
 	def create
@@ -22,13 +32,17 @@ class HintsController < ApplicationController
 		if lecturer_signed_in?
 			p.owner_id = current_lecturer.id
 			p.owner_type = "lecturer"
+			p.category = false
+			p.model_answer_id = @@answer_id
 		else
 			p.owner_id = current_teaching_assistant.id
 			p.owner_type = "teaching_assistant"
+			p.category = false
+			p.model_answer_id = @@answer_id
 		end
 
 		if p.save
-			redirect_to :action => "new", :id => p.id
+			redirect_to :controller => 'model_answers', :action => 'edit', :id => @@answer_id
 		else 
 			flash.keep[:notice] = "Hint is missing paramaters"
 			redirect_to :back
@@ -38,14 +52,10 @@ class HintsController < ApplicationController
 	# [Adding Helping Hints - 4.12]
 	# Passes the input of the form as paramaters for create action to use it
 	# Parameters:
-	#   category: hint's category
 	#   submission_counter: hint's submission counter
 	#   message: hint's message
 	# Returns: params to create action
 	# Author: Mohamed Fadel
 	def permitCreate
-		params.require(:Hint).permit(:category, :submission_counter, :message)
+		params.require(:Hint).permit(:submission_counter, :message)
 	end
-	def create_new
-	end	
-end
