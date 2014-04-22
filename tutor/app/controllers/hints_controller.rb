5da1479aacdbbd53898365b202bc51a7e394a2f8
Mimi <mimi@ubuntu.(none)>
2014-04-22 13:22:12 +0200

    modifying hintsCntroller

--- tutor/app/controllers/hints_controller.rb
+++ tutor/app/controllers/hints_controller.rb
@@ -1,39 +1,32 @@
 class HintsController < ApplicationController
-	#t.text     "message"
-    #t.boolean  "category"
-    #t.integer  "submission_counter"
-    #t.integer  "model_answer_id"
-    #t.integer  "owner_id"
-    #t.string   "owner_type"
 
-	
     def show
-		@new_Tip = Problem.find_by_id(params[:id])
+		@new_Tip = Hint.find_by_id(params[:id])
 	end
    
     # [Edit helping hints - Story 4.13 ]
 	# Description: This action takes the passed parameters from 
-	#              the creation form, creates a new Hint record
-	#              and assigns it to the respective problem. If the 
-	#              creation fails the user is redirected to the form
+	#              the creation form, edits a Hint record
+	#              which assigned  to the respective problem. If the 
+	#              edit fails the user is redirected to the form
 	#              with a "Failed" message.
 	# Parameters:
 	#	hint_params[]: A list that has all fields entered by the user to in the
 	# 					create_topic_form
 	# Returns: 
-	# 	flash[:notice]: A message indicating the success or failure of the creation
+	# 	flash[:notice]: A message indicating the success or failure of the edit
 	# Author: Mimi
 	def create
-		@new_Tip = Hint.new(hint_params)
-		@new_Tip.category = false
+		@new_Tip = Hint.find_by_id(params[:id])
+		@new_Tip.message = hint_params[:message]
+		@new_Tip.submission_counter = hint_params[:submission_counter]
 		bool = @new_Tip.save
 		if bool == true 
-			flash[:notice] = "Hint successfully created"
-			#redirect_to :action => 'create'
+			flash[:notice] = "Hint successfully edited"
 			redirect_to :back
 		else
 			if @new_Tip.errors.any?
-				flash[:notice] = @new_Tip.errors.full_messages.first
+			flash[:notice] = @new_Tip.errors.full_messages.first
 			end
 			#render "create"
 			redirect_to :back
