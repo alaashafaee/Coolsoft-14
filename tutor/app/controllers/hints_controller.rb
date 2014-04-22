4c9e2967aca160ce413781741e660eaa941e3c72
Nadine Adel <nadine@nadine-Inspiron-N4050.(none)>
2014-04-23 01:30:56 +0200

    Issue 265 modifications in controllers

--- tutor/app/controllers/hints_controller.rb
+++ tutor/app/controllers/hints_controller.rb
@@ -1,4 +1,14 @@
 class HintsController < ApplicationController
-  def index
-  end
+  # [View hints and tips-Story 4.22]
+	# It fetches from database all the previous hints.
+	# Parameters: 
+	#   @hints: All the previous hints that had been entered before.
+	#   @hints_check: All the previous hints that had been entered before to check if it is a hint or a tip.
+	# Return : table with all previous hints
+	# Author: Nadine Adel
+	def index
+
+		@hints = Hint.all
+		@hints_check=Hint.all
+	end
 end
--- tutor/app/controllers/tips_controller.rb
+++ tutor/app/controllers/tips_controller.rb
@@ -1,4 +1,13 @@
 class TipsController < ApplicationController
-  def index
-  end
+  # [View hints and tips-Story 4.22]
+	# It fetches from database all the previous tips.
+	# Parameters: 
+	#   @tips: All the previous tips that had been entered before.
+	#   @tips_check:All the previous tips that had been entered before to check if it is a tip or hint.
+	# Return : Table with all the previous tips.
+	# Author: Nadine Adel
+	def index
+		@tips= Hint.all
+		@tips_check =Hint.all
+	end
 end
