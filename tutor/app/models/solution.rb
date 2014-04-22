641bf4adf5b742733311f00218810047f510bdb6
Rami Khalil <thesavagekoder@gmail.com>
2014-04-22 13:51:39 +0200

    Added file name methods to solution model.

--- tutor/app/models/solution.rb
+++ tutor/app/models/solution.rb
@@ -10,9 +10,34 @@ class Solution < ActiveRecord::Base
 	#Scoops
 
 	#Methods
+	# [Compiler: Compile - Story 3.4, Debugger: Debug - Story 3.6]
+	# Returns the java file name associatd with this solution code.
+	# Parameters:
+	# 	append_extension: A boolean value deciding whether the file extension should be appended.
+	# Returns: The solution's java file name
+	# Author: Rami Khalil
+	def java_file_name append_extension=false
+		file_name = JAVA_PATH + 'st' + student_id.to_s + 'pr' + problem_id.to_s + 'so' + id.to_s
+		file_name += ".java" if append_extension
+		return file_name
+	end
+
+	# [Compiler: Compile - Story 3.4, Debugger: Debug - Story 3.6]
+	# Returns the class file name associatd with this solution code.
+	# Parameters:
+	# 	append_extension: A boolean value deciding whether the file extension should be appended.
+	# Returns: The solution's class file name
+	# Author: Rami Khalil
+	def class_file_name append_extension=false
+		file_name = CLASS_PATH + 'st' + student_id.to_s + 'pr' + problem_id.to_s + 'so' + id.to_s
+		file_name += ".class" if append_extension
+		return file_name
+	end
 
 	#Constants
 	STATUS_SUBMITTED 	= 	0
 	STATUS_ACCEPTED		=	1
 
+	JAVA_PATH	=	'students_solutions/Java/'
+	CLASS_PATH	=	'students_solutions/Class'
 end
