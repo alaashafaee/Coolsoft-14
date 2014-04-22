8935dfaad09a3663eb7f5185b9fafa6209f3a476
Muhammad Mamdouh <mamdou7@mamdou7-Satellite-L500.(none)>
2014-04-22 23:57:27 +0200

    added logic to controller

--- tutor/app/controllers/acknowledgements_controller.rb
+++ tutor/app/controllers/acknowledgements_controller.rb
@@ -1,9 +1,15 @@
 class AcknowledgementsController < ApplicationController
 
 	def new 
+		@course = Course.find(params[:course_id])
 	end
 
 	def create
+		@student = Student.find_by_id(params[:student][:id])
+		@course = Course.find_by_id(params[:course_id])
+		@acknowledgement = Acknowledgement.new
+		@acknowledgement.message = acknowledgement_params[:description]
+
 	end	
 
 
