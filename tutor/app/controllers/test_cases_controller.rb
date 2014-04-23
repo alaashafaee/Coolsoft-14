609933ef184fc089f3abf998c2faa5625836a58d
Nadine Adel <nadine@nadine-Inspiron-N4050.(none)>
2014-04-23 03:54:24 +0200

    adding edit and update in controller

--- tutor/app/controllers/test_cases_controller.rb
+++ tutor/app/controllers/test_cases_controller.rb
@@ -1,4 +1,5 @@
 class TestCasesController < ApplicationController
+	@@problemid = nil
 	# [Add test case-story 4.8]
 	# Shows all the test cases.
 	# Parameters: None
@@ -14,6 +15,8 @@ class TestCasesController < ApplicationController
 	# Returns: None
 	# Author: Lin
 	def new
+		@problem = Problem.find(params[:problem_id])
+		@@problemid =params[:problem_id]
 		@test_case = TestCase.new()
 	end
 
@@ -24,21 +27,51 @@ class TestCasesController < ApplicationController
 	#          In case of failure a flash notice will appear:"Can't add test case!"
 	# Author: Lin
 	def create
+		@problems =Problem.find_by_id(@@problemid)
 		if lecturer_signed_in?
 			@test_case = TestCase.new(post_params)
 			@test_case.owner_id = current_lecturer.id
 			@test_case.owner_type = "lecturer"
+			@test_case.problem_id=@@problemid
 		elsif teaching_assistant_signed_in?
 			@test_case = TestCase.new(post_params)
 			@test_case.owner_id = current_teaching_assistant.id
 			@test_case.owner_type = "teaching assistant"
+			@test_case.problem_id=@@problemid
 		end
 		if @test_case.save
+			@problems.test_cases << @test_case
 			flash[:notice] = "Your test case is now added"
 			redirect_to :controller => 'problems', :action => 'edit', :id => @test_case.problem_id
 		else
 			flash[:notice] = "Your test case cannot added"
 			redirect_to :controller => 'problems', :action => 'edit', :id => @test_case.problem_id
+		end
+	end
+
+	# [Edit test case-story 4.9]
+	# Edit test case 
+	# Parameters: 
+	#    @test_case:Test case to be edited.
+	# Returns: None
+	# Author: Nadine Adel
+	def edit
+		@test_case =TestCase.find(params[:id])
+	end
+
+	# [Edit test case-story 4.9]
+	# Update test case in the database
+	# Parameters: 
+	#    @test_case:Test case to be updated.
+	# Returns: flash message if the test case is updated or not
+	# Author: Nadine Adel
+	def update
+		@test_case = TestCase.find(params[:id])
+		if @test_case.update_attributes(post_params)
+			flash[:notice] = "Your Testcase is now updated"
+			redirect_to :controller => 'problems', :action => 'edit', :id => @test_case.problem_id
+		else
+			flash[:notice] = "Your Answer is not updated"
 		end
 	end
 
