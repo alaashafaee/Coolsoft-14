b28d82d40a7a88801d6ddd8cb71aea875e6a665c
Muhammad Mamdouh <mamdou7@mamdou7-Satellite-L500.(none)>
2014-04-24 00:06:13 +0200

    fixing indentation in seeds

--- tutor/db/seeds.rb
+++ tutor/db/seeds.rb
@@ -164,7 +164,7 @@ puts("# -----------------------Students---------------------------")
 	# since the table has key on (student_id and course_id)then the array will always be 1 elemet [0]
 	Student.find_by_id(2).courses << Course.find_by_id(2)
 	CourseStudent.where(student_id:2, course_id:2)[0].update(share: true)
-		Student.find_by_id(1).courses << Course.find_by_id(2)
+	Student.find_by_id(1).courses << Course.find_by_id(2)
 	CourseStudent.where(student_id:1, course_id:2)[0].update(share: true)
 
 puts("# -----------------------Problems---------------------------")
