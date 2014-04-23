517944d520a329992089a924d7c640d0c7a9e8a6
AmirGeorge <dangerousmiro@gmail.com>
2014-04-24 00:59:44 +0200

    removed extra space

--- tutor/config/schedule.rb
+++ tutor/config/schedule.rb
@@ -1,6 +1,6 @@
 # for this to work type this the terminal before starting server:
 # whenever --update-crontab sendReminders
-set :environment, 'development' 
+set :environment, 'development'
 every 2.days do
 	runner "Student.send_reminder_mails"
 end
