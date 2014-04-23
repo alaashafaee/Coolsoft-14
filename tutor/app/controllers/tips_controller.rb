18afac42d65290177948c771ff133a5542a5ca27
Ahmed Osam <osam@osam-HP-ProBook-450-G1.(none)>
2014-04-23 02:31:14 +0200

    Issue 244 documentation for Adding tip story

--- tutor/app/controllers/tips_controller.rb
+++ tutor/app/controllers/tips_controller.rb
@@ -1,34 +1,34 @@
 class TipsController < ApplicationController
-	def new
-		if(@tips == nil)
-			@tips = Hint.new
-		end
-	end
-
-	def show
-	end
 
 	# [Add tip - Story 4.10]
 	# Allows Lecturer/TA to create a tip to help the student_users while solving a problem.
 	# Parameters: 
 	#   :message is the content of the tip.
 	#   :time is a countdown timer that tip will appear after it ends.
-	# Returns: @tips : a new created tip to specific answer.
+	# Returns: @tip : a new created tip to specific answer.
 	# Author: Ahmed Osam
+
+	def new
+		if(@tip == nil)
+			@tip = Hint.new
+		end
+	end
+	
 	def create
-		@tips = Hint.new
-		@tips.message = tip_params[:message]
-		@tips.time = tip_params[:time]
-		@tips.category = true
-		if @tips.save
-	  		current_lecturer.hints << @tips
-	  		@tip = @tips
-	  		render :action => 'show'
+		@tip = Hint.new
+		@tip.message = tip_params[:message]
+		@tip.time = tip_params[:time]
+		@tip.category = true
+		@tip.model_answer_id = @@answer_id
+		if @tip.save
+			render :action => 'show'
 		else
-	  		render :action=>'new'
+			render :action=>'new'
 		end
 	end
 
+	def show
+	end
 
 	def index
 	end
