24ba46a429799b9fb17af7feb2a92bf1ada4243e
Mohamed Fadel <mohamedfadel@Fadel.(none)>
2014-04-23 13:06:43 +0200

    Fixing code of adding problem hints #210

--- tutor/app/controllers/hints_controller.rb
+++ tutor/app/controllers/hints_controller.rb
@@ -14,11 +14,11 @@ class HintsController < ApplicationController
 	def create
 		new_hint = Hint.new(permitCreate)
 		if lecturer_signed_in?
-			current_lecturer.hints <= new_hint
+			current_lecturer.hints << new_hint
 			new_hint.category = false
 			new_hint.model_answer_id = @@answer_id
 		else
-			current_teaching_assistant.hints <= new_hint
+			current_teaching_assistant.hints << new_hint
 			new_hint.category = false
 			new_hint.model_answer_id = @@answer_id
 		end
