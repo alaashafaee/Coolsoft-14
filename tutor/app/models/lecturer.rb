7b7c6bd18068ad1db050d0a56433b9ecdc7a1f38
Ahmed ELAssuty <ahmed.elassuty214@gmail.com>
2014-04-24 01:34:52 +0200

    Issue #223 fix conventions

--- tutor/app/models/lecturer.rb
+++ tutor/app/models/lecturer.rb
@@ -2,7 +2,7 @@ class Lecturer < ActiveRecord::Base
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable and :omniauthable
 	devise :database_authenticatable, :registerable,
-		 :recoverable, :rememberable, :trackable, :validatable
+		:recoverable, :rememberable, :trackable, :validatable
 
 	#Elasticsearch
 	include Tire::Model::Search
