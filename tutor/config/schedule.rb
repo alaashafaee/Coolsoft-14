5d7ea8c14a53b7ca920874a908a1f3ad99ac189d
AmirGeorge <dangerousmiro@gmail.com>
2014-04-23 23:41:22 +0200

    changed interval to 2 days

--- tutor/config/schedule.rb
+++ tutor/config/schedule.rb
@@ -1,6 +1,6 @@
 # for this to work type this the terminal before starting server:
 # whenever --update-crontab sendReminders
 set :environment, 'development' 
-every 1.minute do
+every 2.days do
 	runner "Student.send_reminder_mails"
 end
