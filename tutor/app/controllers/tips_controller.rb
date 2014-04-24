672a5a86a42dbb748dd39973a8e5814467925e36
Ahmed Osam <osam@osam-HP-ProBook-450-G1.(none)>
2014-04-24 22:30:25 +0200

    Issue #244 adding methods of creating and index with their views

--- tutor/app/controllers/tips_controller.rb
+++ tutor/app/controllers/tips_controller.rb
@@ -1,25 +1,41 @@
 class TipsController < ApplicationController
 
-	# [Add tip - Story 4.10]
-	# Allows Lecturer/TA to create a tip to help the student_users while solving a problem.
-	# Parameters: 
-	#   :message is the content of the tip.
-	#   :time is a countdown timer that tip will appear after it ends.
-	# Returns: @tip : a new created tip to specific answer.
+# [Add tip - Story 4.10]
+	# Allows Lecturer/TA to create a tip to help the student_users while solving a problem
+	# Parameters:
+	#     none
+	# Returns:
+	#     @tip: a new created tip to specific answer
 	# Author: Ahmed Osam
-
 	def new
 		if(@tip == nil)
+			session[:model_answer_id] = params[:model_answer_id]
 			@tip = Hint.new
 		end
 	end
 	
+	# [Add tip - Story 4.10]
+	# Allows Lecturer/TA to create a tip to help the student_users while solving a problem
+	# Parameters: 
+	#     :message => is the content of the tip
+	#     :time => is a countdown timer that tip will appear after it ends
+	#     :model_answer_id => is the id of the answer the tip related to
+	# Returns:
+	#     @tip: a new created tip to specific answer
+	# Author: Ahmed Osam
 	def create
 		@tip = Hint.new
 		@tip.message = tip_params[:message]
 		@tip.time = tip_params[:time]
 		@tip.category = true
-		@tip.model_answer_id = @@answer_id
+		@tip.model_answer_id = session[:model_answer_id]
+		if lecturer_signed_in?
+			@tip.owner_type = "lecturer"
+			@tip.owner_id = current_lecturer.id
+		elsif teaching_assistant_signed_in?
+			@tip.owner_type = "teaching assistant"
+			@tip.owner_id = current_teaching_assistant.id
+		end
 		if @tip.save
 			render :action => 'show'
 		else
@@ -27,10 +43,13 @@ class TipsController < ApplicationController
 		end
 	end
 
+
 	def show
 	end
 
 	def index
+		@tips= Hint.all
+		@tips_check =Hint.all
 	end
 
 	def destroy
@@ -42,7 +61,7 @@ class TipsController < ApplicationController
 	def update
 	end
 
-  	private
+	private
 	def tip_params 
 		params.require(:tip).permit(:message, :time)
 	end
