01bdcff056e88c8448b23fe7f27cbe95f242941f
Ahmed ELAssuty <ahmed.elassuty214@gmail.com>
2014-04-24 15:16:07 +0200

    Issue #223 fix conventions

--- tutor/app/models/concerns/searchable.rb
+++ tutor/app/models/concerns/searchable.rb
@@ -1,13 +1,13 @@
 module Searchable
 	extend ActiveSupport::Concern
 
+	#class Methods
+
 	# [Simple Search - Story 1.22]
 	# search for users
 	# Parameters: keyword
 	# Returns: A hash with search results according to the keyword
 	# Author: Ahmed Elassuty
-
-	#class Methods
 	module ClassMethods
 		def simple_search(keyword)
 			self.where("name LIKE ? or email LIKE ?", "%#{keyword}%", "%#{keyword}%") if keyword.present?
