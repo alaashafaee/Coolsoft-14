d4cf212104eb1bcb0b0ecd93dabe058bebd73e22
Mahdi <mahdi@Mahdi-HP-EliteBook-8440p.(none)>
2014-04-24 02:26:21 +0200

    Issue #248 Documentation of methods

--- tutor/app/controllers/students_controller.rb
+++ tutor/app/controllers/students_controller.rb
@@ -1,5 +1,7 @@
 class StudentsController < ApplicationController
-
+#Story 5.3 [Performance of a student]
+#These methods retrieve variables from tables in the database
+#Author : Mahdi
   def get_performance
   	@solved = Attempt.where(student_id:params[:id], success:true).select("DISTINCT problem_id").count
   	@failed = Attempt.where(student_id:params[:id], failure:true).select("DISTINCT problem_id").count
