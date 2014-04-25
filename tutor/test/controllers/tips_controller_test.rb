066da03f778216bf2efcef7a43e3194b6a46bad7
Nadine Adel <nadine@nadine-Inspiron-N4050.(none)>
2014-04-25 16:10:19 +0200

    Issue 265 fixing conflicts

--- tutor/test/controllers/tips_controller_test.rb
+++ tutor/test/controllers/tips_controller_test.rb
@@ -1,8 +1,7 @@
 require 'test_helper'
 
 class TipsControllerTest < ActionController::TestCase
-<<<<<<< HEAD
-=======
+
   test "should get new" do
     get :new
     assert_response :success
@@ -18,14 +17,11 @@ class TipsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
->>>>>>> 021865fc9b2481b46a08f9caa94a32b7d8db7850
   test "should get index" do
     get :index
     assert_response :success
   end
 
-<<<<<<< HEAD
-=======
   test "should get edit" do
     get :edit
     assert_response :success
@@ -35,6 +31,5 @@ class TipsControllerTest < ActionController::TestCase
     get :destroy
     assert_response :success
   end
-
->>>>>>> 021865fc9b2481b46a08f9caa94a32b7d8db7850
+  
 end
