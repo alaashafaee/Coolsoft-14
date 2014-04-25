651807554e7c67488883ce89e0f08b3c2bd4c84e
Ahmed Osam <osam@osam-HP-ProBook-450-G1.(none)>
2014-04-25 14:39:50 +0200

    Issue #342 fixing documentation

--- tutor/app/controllers/tips_controller.rb
+++ tutor/app/controllers/tips_controller.rb
@@ -45,11 +45,11 @@ class TipsController < ApplicationController
 
 	# [Show tip - Story 4.23]
 	# Show the content of the tip that was created or edited with edit and delete buttons
-	# Parameters: 
-	#     model_answer_id: The model answer id
-	#     id: tip id that should be viewed
+	# Parameters:
+	#	model_answer_id: The model answer id
+	#	id: tip id that should be viewed
 	# Returns: 
-	#     @tip: tip that will be viewed
+	#	@tip: tip that will be viewed
 	# Author: Ahmed Osam
 	def show
 		@tip = Hint.find_by_id(params[:id])
