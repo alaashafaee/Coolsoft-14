33478e94e44f921b44b25a84a3b1f7c50aaa9b9f
Khaled <khaled@Khaled-L.(none)>
2014-04-25 17:49:36 +0200

    Issue #380 Fixed conventions

--- tutor/app/controllers/application_controller.rb
+++ tutor/app/controllers/application_controller.rb
@@ -9,66 +9,66 @@ class ApplicationController < ActionController::Base
 
 	private
 
-	# [User Authentication Advanced - Story 5.9, 5.10, 5.11, 5.14, 5.15]
-	# Permits some fields to be passed through sign up forms to update the lecturer,
-	# 	student, and teaching_assistant models
-	# Parameters: None
-	# Returns: None
-	# Author: Khaled Helmy
-	def update_sanitized_params
-		if "#{resource_name}" == "lecturer"
-			devise_parameter_sanitizer.for(:sign_up) {
-				|u| u.permit(:name, :email, :password, :password_confirmation, :gender, :dob, :degree, 
-					:university, :department, :profile_image, :profile_image_cache)
-			}
-		elsif "#{resource_name}" == "student"
-			devise_parameter_sanitizer.for(:sign_up) {
-				|u| u.permit(:name, :email, :password, :password_confirmation, :gender, :dob, :university, 
-					:faculty, :major, :semester, :advising, :probation, :profile_image, :profile_image_cache)
-			}
-		elsif "#{resource_name}" == "teaching_assistant"
-			devise_parameter_sanitizer.for(:sign_up) {
-				|u| u.permit(:name, :email, :password, :password_confirmation, :gender, :dob,
-					:graduated_from, :graduated_year, :degree, :university, :department, 
-					:profile_image, :profile_image_cache)
-			}
+		# [User Authentication Advanced - Story 5.9, 5.10, 5.11, 5.14, 5.15]
+		# Permits some fields to be passed through sign up forms to update the lecturer,
+		# 	student, and teaching_assistant models
+		# Parameters: None
+		# Returns: None
+		# Author: Khaled Helmy
+		def update_sanitized_params
+			if "#{resource_name}" == "lecturer"
+				devise_parameter_sanitizer.for(:sign_up) {
+					|u| u.permit(:name, :email, :password, :password_confirmation, :gender, :dob, :degree, 
+						:university, :department, :profile_image, :profile_image_cache)
+				}
+			elsif "#{resource_name}" == "student"
+				devise_parameter_sanitizer.for(:sign_up) {
+					|u| u.permit(:name, :email, :password, :password_confirmation, :gender, :dob, :university, 
+						:faculty, :major, :semester, :advising, :probation, :profile_image, :profile_image_cache)
+				}
+			elsif "#{resource_name}" == "teaching_assistant"
+				devise_parameter_sanitizer.for(:sign_up) {
+					|u| u.permit(:name, :email, :password, :password_confirmation, :gender, :dob,
+						:graduated_from, :graduated_year, :degree, :university, :department, 
+						:profile_image, :profile_image_cache)
+				}
+			end
 		end
-	end
 
-	# [User Authentication Advanced - Story 5.9, 5.10, 5.11, 5.14, 5.15]
-	# Checks which type of users is currently signed in and if there
-	# 	is any, it blocks other types of users from using the 
-	# 	authentication system
-	# Parameters: None
-	# Returns: None
-	# Author: Khaled Helmy
-	def check_resource
-		if student_signed_in?
-			if "#{resource_name}" == "lecturer" or "#{resource_name}" == "teaching_assistant"
-				redirect_to :root
-			end
-		elsif lecturer_signed_in?
-			if "#{resource_name}" == "student" or "#{resource_name}" == "teaching_assistant"
-				redirect_to :root
-			end
-		elsif teaching_assistant_signed_in?
-			if "#{resource_name}" == "lecturer" or "#{resource_name}" == "student"
-				redirect_to :root
+		# [User Authentication Advanced - Story 5.9, 5.10, 5.11, 5.14, 5.15]
+		# Checks which type of users is currently signed in and if there
+		# 	is any, it blocks other types of users from using the 
+		# 	authentication system
+		# Parameters: None
+		# Returns: None
+		# Author: Khaled Helmy
+		def check_resource
+			if student_signed_in?
+				if "#{resource_name}" == "lecturer" or "#{resource_name}" == "teaching_assistant"
+					redirect_to :root
+				end
+			elsif lecturer_signed_in?
+				if "#{resource_name}" == "student" or "#{resource_name}" == "teaching_assistant"
+					redirect_to :root
+				end
+			elsif teaching_assistant_signed_in?
+				if "#{resource_name}" == "lecturer" or "#{resource_name}" == "student"
+					redirect_to :root
+				end
 			end
 		end
-	end
 
-	# [User Authentication Advanced - Story 5.9, 5.10, 5.11, 5.14, 5.15]
-	# Checks if a user is signed-in in order to be used in authentication over different pages
-	# 	where they are redirected to homepage if they aren't authenticated
-	# Parameters: None
-	# Returns: None
-	# Author: Khaled Helmy
-	def authenticate!
-		unless signed_in? or params[:controller].include?"devise"
-			flash[:notice] = "You're not logged in!"
-			redirect_to :root
+		# [User Authentication Advanced - Story 5.9, 5.10, 5.11, 5.14, 5.15]
+		# Checks if a user is signed-in in order to be used in authentication over different pages
+		# 	where they are redirected to homepage if they aren't authenticated
+		# Parameters: None
+		# Returns: None
+		# Author: Khaled Helmy
+		def authenticate!
+			unless signed_in? or params[:controller].include?"devise"
+				flash[:notice] = "You're not logged in!"
+				redirect_to :root
+			end
 		end
-	end
 
 end
