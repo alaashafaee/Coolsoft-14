62f35f5d3bcdf5c3708d26ad787f41fb7de935ec
Rami Khalil <thesavagekoder@gmail.com>
2014-04-25 22:51:08 +0200

    Fixed controllers.

--- tutor/app/controllers/students_controller.rb
+++ tutor/app/controllers/students_controller.rb
@@ -1,53 +1,5 @@
 class StudentsController < ApplicationController
 
-	#[Performance of a student - Story 5.3]
-	#This method retrieve variables from tables in the database
-	#Parameters:
-	#params: A hash of the request URL attributes
-	#success, failure, incomplete: Booleans represent the status of the problem
-	#@solved: [int] The number of solved problems
-	#@failed: [int] The number of failed problems
-	#@incomplete: [int] The number of incomplete problems
-	#Author : Mahdi
-	def get_performance
-		@solved = Attempt.where(student_id:params[:id], success:true).select("DISTINCT problem_id").count
-		@failed = Attempt.where(student_id:params[:id], failure:true).select("DISTINCT problem_id").count
-		@incomplete = Attempt.where(student_id:params[:id], incomplete:true).select("DISTINCT problem_id").count
-	end
-
-	#[Performance of a student - Story 5.3]
-	#This method retrieve variables from tables in the database
-	#Parameters:
-	#params: A hash of the request URL attributes
-	#Returns:
-	#An array of solved problems
-	#Author : Mahdi
-	def solved_problems
-		@solved_list = Attempt.where(student_id:params[:id], success:true).select("DISTINCT problem_id")
-	end
-
-	#[Performance of a student - Story 5.3]
-	#This method retrieve variables from tables in the database 
-	#Parameters:
-	#params: A hash of the request URL attributes
-	#Returns:
-	#An array of failed problems
-	#Author : Mahdi
-	def failed_problems
-		@failure_list = Attempt.where(student_id:params[:id], failure:true).select("DISTINCT problem_id")
-	end
-
-	#[Performance of a student - Story 5.3]
-	#This method retrieve variables from tables in the database
-	#Parameters:
-	#params: A hash of the request URL attributes
-	#Returns:
-	#An array of incomplete problems  
-	#Author : Mahdi  
-	def incomplete_problems
-		@incomplete_list = Attempt.where(student_id:params[:id], incomplete:true).select("DISTINCT problem_id")
-	end
-
 	# [Profile - Story 5.8]
 	# Displays the profile of the student chosen
 	# Parameters:
@@ -58,4 +10,5 @@ class StudentsController < ApplicationController
 		@student = Student.find(params[:id])
 		@courses = @student.courses.order("created_at desc")
 	end
+	
 end
