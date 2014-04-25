e8c02661d94aa77b620892cf68717571586b8061
Mahdi <mahdi@Mahdi-HP-EliteBook-8440p.(none)>
2014-04-25 23:05:32 +0200

    fixing Documentation

--- tutor/app/controllers/students_controller.rb
+++ tutor/app/controllers/students_controller.rb
@@ -1,49 +1,50 @@
 class StudentsController < ApplicationController
 
-	#[Performance of a student - Story 5.3]
-	#This method retrieve variables from tables in the database
-	#Parameters:
-	#params: A hash of the request URL attributes
-	#success, failure, incomplete: Booleans represent the status of the problem
-	#@solved: [int] The number of solved problems
-	#@failed: [int] The number of failed problems
-	#@incomplete: [int] The number of incomplete problems
-	#Author : Mahdi
+	# [Performance of a student - Story 5.3]
+	# This method retrieve variables from tables in the database
+	# Parameters:
+	# 	params: A hash of the request URL attributes
+	# 	success, failure, incomplete: Booleans represent the status of the problem
+	# Returns:
+	# 	@solved: [int] The number of solved problems
+	# 	@failed: [int] The number of failed problems
+	# 	@incomplete: [int] The number of incomplete problems
+	# Author : Mahdi
 	def get_performance
 		@solved = Attempt.where(student_id:params[:id], success:true).select("DISTINCT problem_id").count
 		@failed = Attempt.where(student_id:params[:id], failure:true).select("DISTINCT problem_id").count
 		@incomplete = Attempt.where(student_id:params[:id], incomplete:true).select("DISTINCT problem_id").count
 	end
 
-	#[Performance of a student - Story 5.3]
-	#This method retrieve variables from tables in the database
-	#Parameters:
-	#params: A hash of the request URL attributes
-	#Returns:
-	#An array of solved problems
-	#Author : Mahdi
+	# [Performance of a student - Story 5.3]
+	# This method retrieve variables from tables in the database
+	# Parameters:
+	# 	params: A hash of the request URL attributes
+	# Returns:
+	# 	An array of solved problems
+	# Author : Mahdi
 	def solved_problems
 		@solved_list = Attempt.where(student_id:params[:id], success:true).select("DISTINCT problem_id")
 	end
 
-	#[Performance of a student - Story 5.3]
-	#This method retrieve variables from tables in the database 
-	#Parameters:
-	#params: A hash of the request URL attributes
-	#Returns:
-	#An array of failed problems
-	#Author : Mahdi
+	# [Performance of a student - Story 5.3]
+	# This method retrieve variables from tables in the database 
+	# Parameters:
+	# 	params: A hash of the request URL attributes
+	# Returns:
+	# 	An array of failed problems
+	# Author : Mahdi
 	def failed_problems
 		@failure_list = Attempt.where(student_id:params[:id], failure:true).select("DISTINCT problem_id")
 	end
 
-	#[Performance of a student - Story 5.3]
-	#This method retrieve variables from tables in the database
-	#Parameters:
-	#params: A hash of the request URL attributes
-	#Returns:
-	#An array of incomplete problems  
-	#Author : Mahdi  
+	# [Performance of a student - Story 5.3]
+	# This method retrieve variables from tables in the database
+	# Parameters:
+	# 	params: A hash of the request URL attributes
+	# Returns:
+	# 	An array of incomplete problems  
+	# Author : Mahdi  
 	def incomplete_problems
 		@incomplete_list = Attempt.where(student_id:params[:id], incomplete:true).select("DISTINCT problem_id")
 	end
