29fd375b5d40fcc7402acc8300a4a6a217706fa8
ahmed93 <hero.20112012@gmail.com>
2014-05-06 16:50:16 +0200

    removing migratoins

--- tutor/db/migrate/20140505100813_join_table_contest_problem.rb
+++
@@ -1,9 +0,0 @@
-class JoinTableContestProblem < ActiveRecord::Migration
-  def change
-  	create_join_table :contests, :problems do |t|
-      # t.index [:contest_id, :problem_id]
-      # t.index [:problem_id, :contest_id]
-  	end
-  	add_index(:contests_problems, [:contest_id, :problem_id], :unique => true)
-  end
-end
