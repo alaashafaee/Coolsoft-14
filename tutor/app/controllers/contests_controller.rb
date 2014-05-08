9cd89efcaec98075efc6afe6fba3118f2077ac73
AmirGeorge <dangerousmiro@gmail.com>
2014-05-08 22:48:58 +0200

    #442 added create action in contests controller

--- tutor/app/controllers/contests_controller.rb
+++ tutor/app/controllers/contests_controller.rb
@@ -1,2 +1,21 @@
 class ContestsController < ApplicationController
+
+	def create
+		@new_contest  = Contest.new
+		@new_contest.title = contest_params[:title]
+		@new_contest.course_id = contest_params[:course_id]
+		@new_contest.description = contest_params[:description]
+		@new_contest.expiration_date = contest_params[:expiration_date]
+		@new_contest.start_date = contest_params[:start_date]
+		if lecturer_signed_in?
+			@new_contest.owner_id = current_lecturer.id
+		elsif teaching_assistant_signed_in?
+			@new_contest.owner_id = current_teaching_assistant.id
+		if @new_contest.save
+			flash[:success_creation]= "Contest added."
+			redirect_to :action => 'index'
+		else 
+			render :action=>'new'
+		end
+
 end
