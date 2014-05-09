640c6c973f6581937bad4b7c1390484ccd7be8ec
AmirGeorge <dangerousmiro@gmail.com>
2014-05-09 23:41:32 +0200

    #442 fixed database insertion problem

--- tutor/app/controllers/contests_controller.rb
+++ tutor/app/controllers/contests_controller.rb
@@ -9,21 +9,26 @@ class ContestsController < ApplicationController
 	def create
 		@new_contest  = Contest.new
 		@new_contest.title = contest_params[:title]
-		@new_contest.course_id = contest_params[:course_id]
+		#@new_contest.course_id = contest_params[:course_id]
 		@new_contest.description = contest_params[:description]
-		@new_contest.expiration_date = contest_params[:expiration_date]
-		@new_contest.start_date = contest_params[:start_date]
-		if lecturer_signed_in?
-			@new_contest.owner_id = current_lecturer.id
-		elsif teaching_assistant_signed_in?
-			@new_contest.owner_id = current_teaching_assistant.id
-		end
+		#@new_contest.expiration_date = contest_params[:expiration_date]
+		#@new_contest.start_date = contest_params[:start_date]
+		#if lecturer_signed_in?
+		#	@new_contest.owner_id = current_lecturer.id
+		#elsif teaching_assistant_signed_in?
+		#	@new_contest.owner_id = current_teaching_assistant.id
+		#end
 		if @new_contest.save
 			flash[:success_creation]= "Contest added."
 			redirect_to :action => 'index'
-		else 
+		else
 			render :action=>'new'
 		end
 	end
+
+	private
+		def contest_params 
+			params.require(:contest).permit(:name,:description)
+		end
 
 end
