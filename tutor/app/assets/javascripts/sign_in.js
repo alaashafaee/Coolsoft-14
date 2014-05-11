b966850a576f65bf72c8843671a029d12c2cfee4
Mohab <mohab@mohab-Aspire-4750.(none)>
2014-05-11 21:27:49 +0200

    Issue #466 Adding sign_in dialog for student

---
+++ tutor/app/assets/javascripts/sign_in.js
@@ -0,0 +1,8 @@
+// [Design layout]
+// pops up a dialog containing tabs for each type of user to sign up
+// Parameters: none
+// Returns: none
+// Author: Mohab Ghanim
+function sign_in_dialog(){
+	$('.sign_in_dialog').bPopup();
+}
