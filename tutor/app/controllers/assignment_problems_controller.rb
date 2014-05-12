ffc271fbfe2699cf07e8d7449a3665f24736405a
Linsara <linsara1@gmail.com>
2014-05-12 13:54:51 +0200

    Issue #423 added edit button for the TA view of assignments

--- tutor/app/controllers/assignment_problems_controller.rb
+++ tutor/app/controllers/assignment_problems_controller.rb
@@ -9,8 +9,27 @@ class AssignmentProblemsController < ApplicationController
 			#@course = @topic.course
 			@assignment = @problem.assignment
 			@course = @assignment.course
-			puts(@course)
-			puts(@assignment)
+			#puts(@course)
+			#puts(@assignment)
+			@can_edit = @course.can_edit(current_lecturer)
+			@can_edit||= @course.can_edit(current_teaching_assistant)
+		end
+	end
+
+	def edit
+		@problem = AssignmentProblem.find_by_id(params[:id])
+		if @problem.nil?
+			render "problem_not_found"
+		else
+			#@track = @problem.track
+			#@topic = @track.topic
+			#@course = @topic.course
+			@assignment = @problem.assignment
+			@course = @assignment.course
+			#puts(@course)
+			#puts(@assignment)
+			@can_edit = @course.can_edit(current_lecturer)
+			@can_edit||= @course.can_edit(current_teaching_assistant)
 		end
 	end
 end
