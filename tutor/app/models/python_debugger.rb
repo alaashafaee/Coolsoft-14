b0ab5f97cb8929ac6f02b2ed00e17b165ae361c6
Khaled <khaled@Khaled-L.(none)>
2014-05-12 22:01:20 +0200

    Issue #452 Added documentation

--- tutor/app/models/python_debugger.rb
+++ tutor/app/models/python_debugger.rb
@@ -3,6 +3,14 @@ class PythonDebugger
 
 	#Methods
 
+	# [View Variables Python Code - Story X.8]
+	# Fetches the variables found in the class and returns
+	# 	a list of all variables in the class with their
+	#	associated values
+	# Parameters: none
+	# Returns:
+	# 	An array. It contains the list of variables and their values
+	# Author: Khaled Helmy
 	def get_variables
 		local_variables = []
 		class_variables = get_class_variables
@@ -16,6 +24,16 @@ class PythonDebugger
 		return local_variables
 	end
 
+	# [View Variables Python Code - Story X.8]
+	# Fetches the the stack trace of the code at the current state
+	# 	where the call stack is examined which contains the list of
+	# 	methods which did not finish its execution and thus did not
+	# 	return yet
+	# Parameters: none
+	# Returns:
+	# 	An array. It contains the list of methods in the call stack
+	# 		which have not returned yet
+	# Author: Khaled Helmy
 	def get_stack_trace
 		stack_trace = []
 		input "w"
