fcfa5ac71ee79e976bf536a59c6d246fb946322b
AmirGeorge <dangerousmiro@gmail.com>
2014-05-13 00:43:57 +0200

    #443 added delete action in contests controller

--- tutor/app/controllers/contests_controller.rb
+++ tutor/app/controllers/contests_controller.rb
@@ -38,6 +38,29 @@ class ContestsController < ApplicationController
 		end
 	end
 
+	
+	# [Edit Contest - Story 5.17]
+	# Passes instance variable of the contest to be deleted to the
+	# 	delete view and renders the delete view
+	# Parameters: none
+	# Returns: none
+	# Author: Amir George
+	def delete
+		if student_signed_in?
+			render ('public/404')
+		end
+		@contest = Contest.find(params[:id])
+		if lecturer_signed_in?
+			if !current_lecturer.contests.include?(@contest)
+				render ('public/404')
+			end
+		elsif teaching_assistant_signed_in?
+			if !current_teaching_assistant.contests.include?(@contest)
+				render ('public/404')
+			end
+		end
+	end
+
 	# [Edit Contest - Story 5.17]
 	# Defines the parameters for the contest to be permitted from the
 	# 	form view
