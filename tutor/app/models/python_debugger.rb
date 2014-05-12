e738ec1fe286cfd5f09a948e7c6081f3bf1e3d32
Khaled <khaled@Khaled-L.(none)>
2014-05-12 21:58:17 +0200

    Issue #452 Added method for getting variables while debugging

--- tutor/app/models/python_debugger.rb
+++ tutor/app/models/python_debugger.rb
@@ -4,7 +4,16 @@ class PythonDebugger
 	#Methods
 
 	def get_variables
-
+		local_variables = []
+		class_variables = get_class_variables
+		input "dir()"
+		output_buffer = buffer_until_complete
+		output_buffer.each_line do |line|
+			new_line = line[1..-1]
+			list_of_variables = new_line.split(", ");
+			local_variables << list_of_variables
+		end
+		return local_variables
 	end
 
 	def get_stack_trace
