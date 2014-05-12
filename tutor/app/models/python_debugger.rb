ba1a2d67d506275bf33b2e64540336f6b3a3c60e
Khaled <khaled@Khaled-L.(none)>
2014-05-12 21:25:15 +0200

    Issue #452 method stubs base

--- tutor/app/models/python_debugger.rb
+++ tutor/app/models/python_debugger.rb
@@ -3,4 +3,24 @@ class PythonDebugger
 
 	#Methods
 
+	def get_name variable
+
+	end
+
+	def get_value variable
+
+	end
+
+	def get_class_variables
+
+	end
+
+	def get_variables
+
+	end
+
+	def get_stack_trace
+
+	end
+
 end
