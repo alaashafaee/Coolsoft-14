2b6658546a9f763b0cc7979689a272c5e7275e58
Ahmed Akram <a.akram93@gmail.com>
2014-05-13 12:35:41 +0200

    Issue #491, contest controller

--- tutor/app/controllers/contests_controller.rb
+++ tutor/app/controllers/contests_controller.rb
@@ -10,6 +10,13 @@ class ContestsController < ApplicationController
 		@student_id = current_student.id
 		@problem = Cproblem.first
 		@contest = Contest.find_by_id(1)
+
+		code = params[:code]
+		student = current_student.id
+		problem = 1
+		time = 20
+		result = SolutionsLayer.validate "java", code, student, problem, time, "Contest"
+		render json: result
 		if @contest.students.find_by_id(@student_id)
 			@var = "hahahah"
 		else
