1bf1d7d9184b8fa351a1a9dc524c8080609d8de5
Khaled <khaled@Khaled-L.(none)>
2014-05-13 22:50:12 +0200

    Issue #452 Added documentation

--- tutor/app/models/python_debugger.rb
+++ tutor/app/models/python_debugger.rb
@@ -3,6 +3,16 @@ class PythonDebugger
 
 	#Methods
 
+	# [View Variables Python Code - Story X.8]
+	# Checks if a variable is valid. A variable is valid if it is not
+	# 	a module, a function or a reserved word.
+	# Parameters:
+	# 	name: a String containing the variable name.
+	# 	value: a String containing the value of the variable.
+	# Returns:
+	# 	Boolean. It returns true if the variable is valid and
+	# 		false otherwise.
+	# Author: Khaled Helmy
 	def is_valid_variable name, value
 		if name.match("__")
 			return false
@@ -11,6 +21,14 @@ class PythonDebugger
 		return true
 	end
 
+	# [View Variables Python Code - Story X.8]
+	# Gets the instance variable values of an object.
+	# Parameters:
+	# 	name: a String containing the object name.
+	# Returns:
+	# 	String. It returns the values of the instance variables
+	# 		in an object.
+	# Author: Khaled Helmy
 	def get_object_value name
 		result = ""
 		input name + ".__dict__"
@@ -21,6 +39,18 @@ class PythonDebugger
 		return result
 	end
 
+	# [View Variables Python Code - Story X.8]
+	# Evaluates the value of a variable according to its scope whether
+	# 	it is a local variable or a global variable. It uses the helper
+	# 	method "get_object_value" to evaluate the variable value if it
+	# 	is an object.
+	# Parameters:
+	# 	type: a String containing the type of the variable. Either a local
+	# 		variable or a global variable.
+	# 	name: a String containing the name of the variable.
+	# Returns:
+	# 	String. It returns the evaluation of a certain variable.
+	# Author: Khaled Helmy	
 	def get_variable type, name
 		value = ""
 		input type + "[" + name + "]"
@@ -28,13 +58,24 @@ class PythonDebugger
 		output_buffer.each_line do |line|
 			value << line
 		end
-		if is_valid_variable name, value == false
-			return 0
-		elsif value.match("instance")
+		if value.match("instance")
 			value = get_object_value name
 		return value
 	end
 
+	# [View Variables Python Code - Story X.8]
+	# Gets the list of variable names according to the scope whether
+	# 	local or global and evaluates the value of each variable. It
+	# 	uses the helper method "get_variable" to evaluate the value of
+	# 	a given variable and "is_valid_variable" which checks if a variable
+	# 	is valid.
+	# Parameters:
+	# 	type: a String containing the type of variables that need to be fetched.
+	# 		The variables are local or global.
+	# Returns:
+	# 	An array. It contains the list of variables with their corresponding values
+	# 		in the form of "name = value" according to their scope.
+	# Author: Khaled Helmy
 	def get_variables_helper type
 		all_lines = ""
 		result = []
@@ -48,7 +89,7 @@ class PythonDebugger
 		list_of_variables.each do |variable|
 			name = variable
 			value = get_variable type, name
-			unless value == 0
+			if is_valid_variable name, value == true
 				result << name + " = " + value
 			end
 		end
