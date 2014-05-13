e789eaeb997d42d067035c2b363fcc9d6754fc3e
Mimi <mimi@ubuntu.(none)>
2014-05-13 22:03:00 +0200

    #414 add more resources

--- tutor/app/admin/test_case.rb
+++ tutor/app/admin/test_case.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register Solution do
+ActiveAdmin.register TestCase do
 
   
   # See permitted parameters documentation:
---
+++ tutor/students_solutions/st1pr1so5/CoolSoft.java
@@ -0,0 +1,8 @@
+public class CoolSoft {
+	public static void main(String [] args) {
+	    int x = Integer.parseInt(args[0]);
+	    	    int y = Integer.parseInt(args[1]);
+
+		System.out.println(x/y);
+	}
+}
