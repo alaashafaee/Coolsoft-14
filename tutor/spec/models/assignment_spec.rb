ac3020fa185f472715f06fa365fa2555f46ea0ec
Linsara <linsara1@gmail.com>
2014-05-13 11:24:32 +0200

    Issue #423 Added more RSpec tests

--- tutor/spec/models/assignment_spec.rb
+++ tutor/spec/models/assignment_spec.rb
@@ -1,13 +1,19 @@
 require 'spec_helper'
 
 describe Assignment do
-	it 'Invalid assignment without title' do
-		ass = Assignment.new(title:"bbbbb", publish: false , description:"Allows practice on instruction set formats." ,due_date: DateTime.now.to_date)
-		expect(ass.title).not_to eq(nil)
-	end
+	it 'Assignment is valid with a title and description' do
+		assignment = Assignment.new(
+			title: 'rspec assignemt',
+			description: 'This is a test assignemnt',
+			due_date: Date.new(2015,1,1))
+		expect(assignment).to be_valid
+	end 
 
-#	it 'InValid without a title' do
-#		ass = Assignment.new
-#		expect(ass.title).to eq(nil)
-#	end
+	it 'Assignment is invalid without title' do
+		assignment = Assignment.new(
+			description: 'This is a test assignemnt',
+			due_date: Date.new(2015,1,1)
+			)
+		expect(assignment).not_to be_valid
+	end 
 end
