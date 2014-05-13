4f774e28f01a21f20d1f336d1d7f9a756096bfd8
ahmed93 <hero.20112012@gmail.com>
2014-05-13 23:04:49 +0200

    fixing indentaions and convintions

--- tutor/db/migrate/20140513171629_create_notifications.rb
+++ tutor/db/migrate/20140513171629_create_notifications.rb
@@ -1,8 +1,8 @@
 class CreateNotifications < ActiveRecord::Migration
   def change
     create_table :notifications do |t|
-      t.string :message, nil:false
-      t.boolean :seen, defualt: false
+      t.string :message, null:false
+      t.boolean :seen, default: false
 
       t.references :receiver, polymorphic: true
       t.timestamps
