310aca415a697c7bc8dafa8b94146cafde3e51da
Ahmed Osam <osam@osam-HP-ProBook-450-G1.(none)>
2014-05-14 00:08:45 +0200

    Issue #506 adding new & create methods in hints controller

--- tutor/app/controllers/hints_controller.rb
+++ tutor/app/controllers/hints_controller.rb
@@ -56,6 +56,56 @@ class HintsController < ApplicationController
 		end
 	end
 
+	# [Add hint - Story 4.10]
+	# Allows Lecturer/TA to create a hint to help the student_users while solving a problem
+	# Parameters: none
+	# Returns:
+	#	@hint: a new created hint to specific answer
+	# Author: Ahmed Osam
+	def new
+		@hint = Hint.new
+		session[:track_id] = params[:track_id]
+		if(@hint == nil)
+			session[:model_answer_id] = params[:model_answer_id]
+			@model_answer = ModelAnswer.find_by_id(session[:model_answer_id])
+		end
+	end
+
+	# [Add hint - Story 4.10]
+	# Allows Lecturer/TA to create a hint to help the student_users while solving a problem
+	# Parameters: 
+	#	message: is the content of the hint
+	#	time: is a countdown timer that hint will appear after it ends
+	#	model_answer_id: is the id of the answer the hint related to
+	# Returns:
+	#	@hint: a new created hint to specific answer
+	# Author: Ahmed Osam
+	def create
+		@hint = Hint.new
+		@model_answer = ModelAnswer.find_by_id(session[:model_answer_id])
+		@hint.message = hint_params[:message]
+		@hint.time = 0
+		@hint.category = false
+		@hint.model_answer_id = session[:model_answer_id]
+		@hint.submission_counter = hint_params[:submission_counter]
+		if lecturer_signed_in?
+			@hint.owner_type = "lecturer"
+			@hint.owner_id = current_lecturer.id
+		elsif teaching_assistant_signed_in?
+			@hint.owner_type = "teaching assistant"
+			@hint.owner_id = current_teaching_assistant.id
+		end
+		if @hint.save
+			redirect_to :controller => 'hints', :action => 'new',
+			:model_answer_id => session[:model_answer_id], 
+			:problem_id => session[:problem_id],
+			:track_id => session[:track_id]
+		else
+			render :action => 'new'
+		end
+	end
+
+
  	# [Edit helping hints - Story 4.13 ]
 	# Description: 
 	#	take the parameters from the from
