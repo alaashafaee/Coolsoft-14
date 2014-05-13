dc4e7a1430943f7358aa6d6be1169c09c24c6b6e
Ahmed Osam <osam@osam-HP-ProBook-450-G1.(none)>
2014-05-13 23:46:06 +0200

    Issue #506 editing routes file adding missing routes

--- tutor/config/routes.rb
+++ tutor/config/routes.rb
@@ -32,6 +32,12 @@ Tutor::Application.routes.draw do
 	get "tips/edit"
 	get "tips/destroy"	
 	post "tips/:id/edit" => 'tips#update'
+	get "problems/destroy"
+	get "test_cases/edit"
+	get "test_cases/destroy"
+	get "model_answers/edit"
+	get "hints/edit"
+	get "model_answers/destroy"
 
 	# You can have the root of your site routed with "root"
 	root 'site#index'
