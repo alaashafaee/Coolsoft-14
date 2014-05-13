bc5aa2448bcbbd6891a52c9609b6ce1b7b22646d
Ahmed ELAssuty <ahmed.elassuty214@gmail.com>
2014-05-14 00:00:53 +0200

    Issue #434 resources action new

--- tutor/app/controllers/resources_controller.rb
+++ tutor/app/controllers/resources_controller.rb
@@ -1,6 +1,19 @@
 class ResourcesController < ApplicationController
 
 	# [Course Resources - Story 1.25]
+	# Initialize resource
+	# Parameters: no parameters
+	# Returns: New Resource object
+	# Author: Ahmed Elassuty
+	def new
+		@resource = Resource.new
+		@course = Course.find_by_id(params[:course_id])
+		respond_to do |format|
+			format.html
+		end
+	end
+
+	# [Course Resources - Story 1.25]
 	# add resources to a course
 	# Parameters: hash of the resources attributes
 	# Returns: redirect to course resources page
