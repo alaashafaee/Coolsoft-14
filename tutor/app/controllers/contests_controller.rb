f7878740959afdd1373b24d93f9b2eeed8a831ac
AmirGeorge <dangerousmiro@gmail.com>
2014-05-14 04:13:37 +0200

    #447 added documentation and removed unnecessary instance variable

--- tutor/app/controllers/contests_controller.rb
+++ tutor/app/controllers/contests_controller.rb
@@ -1,26 +1,33 @@
 class ContestsController < ApplicationController
+	
+	# [Add Contest Problem - Story 5.18]
+	# Passes instance variable of the contest to the add
+	# 	problems view and renders the add problems view
+	# Parameters: none
+	# Returns: none
+	# Author: Amir George
 	def add_problems
 		if student_signed_in?
 			render ('public/404')
 		end
 		#add support to add problems from create view not only edit view
 		@contest = Contest.find(params[:id])
-		if lecturer_signed_in? 
-			if !current_lecturer.contests.include?(@contest)
-				render ('public/404')
-			else
-				@owner = current_lecturer
-			end
-		elsif teaching_assistant_signed_in?
-			if !current_teaching_assistant.contests.include?(@contest)
-				render ('public/404')
-			else 
-				@owner = current_teaching_assistant
-			end
+		if lecturer_signed_in?  &&
+			!current_lecturer.contests.include?(@contest)
+			render ('public/404')
+		elsif teaching_assistant_signed_in? &&
+			!current_teaching_assistant.contests.include?(@contest)
+			render ('public/404')
 		end
 		@problems = Cproblem.all
 	end
 
+	# [Add Contest Problem - Story 5.18]
+	# Adds a certain problem to the contest if it was not
+	# 	added before
+	# Parameters: none
+	# Returns: none
+	# Author: Amir George
 	def add
 		@contest = Contest.find(params[:id])
 		@problem = Cproblem.find(params[:problem_id])
