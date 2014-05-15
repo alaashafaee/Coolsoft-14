99aa575a31c82bdbcade30989484955be78e90a1
Khaled <khaled@Khaled-L.(none)>
2014-05-15 19:31:07 +0200

    Issue #450 Added documentation

--- tutor/app/controllers/contests_controller.rb
+++ tutor/app/controllers/contests_controller.rb
@@ -4,6 +4,23 @@ class ContestsController < ApplicationController
 	
 	private
 
+		# [Contest Timer - Story 5.28]
+		# Checks the state of the timer based on the date and time of a specific contest
+		# 	and affecting the behaviour of the timer and the message that accompanies it
+		# 	based on the timing of the contest. If the contest didn't start yet, the timer
+		# 	will be set to countdown till the start of the contest and the message appearing
+		# 	will be 'Contest starts in'. If the contest has started, the timer will be set
+		# 	to countdown till the end of the contest and the message will be 'Contest ends in'.
+		# 	If the contest has ended, the timer will disappear and the message will be
+		# 	'Contest has finished!'.
+		# Parameters: none
+		# Returns:
+		# 	@timer: a String representation of a DateTime object representing the date
+		# 		and the time of the contest that will be used by the timer.
+		# 	@message: a String containing the message accompanying the timer.
+		# 	@del: a Boolean indicating if the contest has finished which affects the
+		# 		behaviour of the timer; whether to remove it or not.
+		# Author: Khaled Helmy
 		def validate_timer
 			@contest = Contest.find(params[:id])
 			contest_start = (@contest.start_date.to_s + " " + @contest.start_time.to_s).to_datetime
