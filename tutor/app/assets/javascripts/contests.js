6e4d064cefc4961772a4890662bb8ce75ae2eeb4
Khaled <khaled@Khaled-L.(none)>
2014-05-15 18:41:00 +0200

    Issue #450 Added the behaviour of reloading the page after the timer ends

--- tutor/app/assets/javascripts/contests.js
+++ tutor/app/assets/javascripts/contests.js
@@ -3,11 +3,15 @@ jQuery(document).on("ready page:load", function() {
 		var timer = $('#temp').data('timer')
 		var destroy = $('#temp').data('del')
 		var count = new Date(timer);
-		$('#countdown').countdown(destroy ? 'destroy' : {until: count, onTick: highlightLast10});
+		$('#countdown').countdown(destroy ? 'destroy' : {until: count, onTick: highlightLast10,
+			onExpiry: reload});
 		function highlightLast10(periods) { 
-			if ($.countdown.periodsToSeconds(periods) === 10) { 
+			if ($.countdown.periodsToSeconds(periods) <= 10) { 
 				$(this).addClass('highlight'); 
 			} 
+		}
+		function reload() {
+			location.reload(true)
 		}
 	});
 });
