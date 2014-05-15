ff3b65c2a69f9270861ae47d834f76775ca9abd6
Nadine Adel <nadine@nadine-Inspiron-N4050.(none)>
2014-05-15 03:00:28 +0200

    Issue #421 conventions assignment views and controller

--- tutor/app/controllers/assignments_controller.rb
+++ tutor/app/controllers/assignments_controller.rb
@@ -1,4 +1,5 @@
 class AssignmentsController < ApplicationController
+
 	def show
 		session[:course_id] = params[:id]
 		@course = Course.find_by_id(session[:id])
@@ -6,18 +7,18 @@ class AssignmentsController < ApplicationController
 
 	def new
 		@course = Course.find_by_id(session[:course_id])
-		@new_assignment = Assignment.new	
+		@new_assignment = Assignment.new
 	end
 
 	def create 
 		@course = Course.find_by_id(session[:course_id])
 		@new_assignment = Assignment.new
-		@new_assignment.title = post_params[:title]
-		@new_assignment.description = post_params[:description]
-		@new_assignment.due_date = post_params[:due_date]
+		@new_assignment.title = assignment_params[:title]
+		@new_assignment.description = assignment_params[:description]
+		@new_assignment.due_date = assignment_params[:due_date]
 		@new_assignment.course_id = session[:course_id]
-		t = DateTime.new(post_params["date(1i)"].to_i, post_params["date(2i)"].to_i,
-			post_params["date(3i)"].to_i )
+		t = DateTime.new(assignment_params["date(1i)"].to_i, assignment_params["date(2i)"].to_i,
+			assignment_params["date(3i)"].to_i )
 		@new_assignment.due_date = t
 		if lecturer_signed_in?
 			@new_assignment.owner_id = current_lecturer.id
@@ -30,20 +31,14 @@ class AssignmentsController < ApplicationController
 			@course.assignments << @new_assignment
 			flash[:success_creation] = "Assignment added."
 			redirect_to :controller => 'assignment_problems', :action => 'new',
-				:id => @new_assignment.id 
+				:id => @new_assignment.id
 		else
 			render :action=>'new', :course_id => params[:course_id]
 		end
 	end
 
-	def index
-	end
-
-	def edit
-	end
-
 	private
-	def post_params
+	def assignment_params
 		params.require(:assignment).permit(:title, :description, :date)
 	end	
 end
