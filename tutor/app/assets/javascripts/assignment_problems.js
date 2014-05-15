56e2492266cf67bb89916f736bac39edc72878a5
Nadine Adel <nadine@nadine-Inspiron-N4050.(none)>
2014-05-15 02:25:52 +0200

    Issue #421 moving script to javascript file

---
+++ tutor/app/assets/javascripts/assignment_problems.js
@@ -0,0 +1,12 @@
+('input[type="checkbox"]').checkbox({
+   buttonStyle: 'btn-link',
+   buttonStyleChecked: 'btn-inverse',
+   checkedClass: 'icon-check',
+   uncheckedClass: 'icon-check-empty',
+   constructorCallback: null,
+   defaultState: false,
+   defaultEnabled: true,
+ 
+   checked: false,
+   enabled: true
+});
