7fba92c61fa8274bee0622c5230636526a7b27f4
Ahmed Atef <Ahmed@Ahmeds-MacBook-Pro-9.local>
2014-05-15 20:40:41 +0200

    #525 Fixed my test cases for the contest show method

--- tutor/spec/controllers/contests_controller_spec.rb
+++ tutor/spec/controllers/contests_controller_spec.rb
@@ -2,21 +2,20 @@ require 'spec_helper'
 
 describe ContestsController do
 	describe "GET show" do
+        before(:each) do
+            @contest1 = Contest.create(title: 'TestContest', description: 'Some description')
+        end
 
-  		
+      	it "assigns the requested contest to the @contest" do
+            ContestsController.skip_before_filter :authenticate!
+        	get :show, id: @contest1.id
+        	expect(assigns(:contest)).to eq(@contest1)
+        end
 
-    	it "assigns the requested contest to the @contest" do
-    	@contest1 = Contest.new(title: 'TestContest', description: 'Some description')
-    	@contest1.save	
-    	get :show, id: @contest1.id
-    	#assigns(:contest).to eq(@contest1)
-    	expect(assigns(:@contest)).to eq(@contest1)
-    	end
-
-    	it "render show template" do
-    	@contest2 = Contest.new(title: 'TestContest', description: 'Some description')	
-		get :show, id: @contest2.id
-    	expect(response).to render_template("show")
-    	end
+        	it "render show template" do
+            ContestsController.skip_before_filter :authenticate!	
+    		get :show, id: @contest1.id
+        	expect(response).to render_template("show")
+        end
     end	
 end
