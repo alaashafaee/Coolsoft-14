d512c36fd815bd7b5301cb5fb01a911387edd7ec
Nadine Adel <nadine@nadine-Inspiron-N4050.(none)>
2014-05-15 02:30:44 +0200

    Issue #421 conventions in js file and index view

--- tutor/app/assets/javascripts/assignment_problems.js
+++ tutor/app/assets/javascripts/assignment_problems.js
@@ -1,12 +1,11 @@
 ('input[type="checkbox"]').checkbox({
-   buttonStyle: 'btn-link',
-   buttonStyleChecked: 'btn-inverse',
-   checkedClass: 'icon-check',
-   uncheckedClass: 'icon-check-empty',
-   constructorCallback: null,
-   defaultState: false,
-   defaultEnabled: true,
- 
-   checked: false,
-   enabled: true
+	buttonStyle: 'btn-link',
+	buttonStyleChecked: 'btn-inverse',
+	checkedClass: 'icon-check',
+	uncheckedClass: 'icon-check-empty',
+	constructorCallback: null,
+	defaultState: false,
+	defaultEnabled: true,
+	checked: false,
+	enabled: true
 });
