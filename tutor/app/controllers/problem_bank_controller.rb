094df62ec1729ada6e0e303cb6e17d042a54d29f
Ahmed Sharaf <ahmed@ahmed-Aspire-E1-571G.(none)>
2014-05-15 16:28:23 +0200

    updated controller

--- tutor/app/controllers/problem_bank_controller.rb
+++ tutor/app/controllers/problem_bank_controller.rb
@@ -29,20 +29,7 @@ class ProblemBankController < ApplicationController
 		@track = Track.find_by_id(params[:track_id])
 		@new_problem = @problem.duplicate
 		@new_problem.duplicated = true
-		if lecturer_signed_in?
-			@flag = true
-			@track.problems.each do |pr| 
-				if pr.title == @new_problem.title
-					flash[:notice]= @new_problem.title,"You already have this problem"
-					@flag = false
-	        	end
-	        end
-	        if @flag == true
-		        @new_problem.track_id = @track.id
-	        	current_lecturer.problems << @new_problem
-	        	flash[:success]= @new_problem.title,"is added successfully"
-       		end
-		elsif teaching_assistant_signed_in?
+		if teaching_assistant_signed_in?
 			@flag = true
 			@track.problems.each do |pr|
 				if pr.title == @new_problem.title
@@ -55,6 +42,19 @@ class ProblemBankController < ApplicationController
 				current_teaching_assistant.problems << @new_problem
 				flash[:success]= @new_problem.title,"is added successfully"
 			end
+		elsif lecturer_signed_in?
+			@flag = true
+			@track.problems.each do |pr| 
+				if pr.title == @new_problem.title
+					flash[:notice]= @new_problem.title,"You already have this problem"
+					@flag = false
+	        	end
+	        end
+	        if @flag == true
+		        @new_problem.track_id = @track.id
+	        	current_lecturer.problems << @new_problem
+	        	flash[:success]= @new_problem.title,"is added successfully"
+       		end
 		end
 		redirect_to :action => 'show', :id => @track.id
 	end	
