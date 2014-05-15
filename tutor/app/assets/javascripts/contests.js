22db0ebd0dfc595ea88b44b8bfa53e576fb9b829
Khaled <khaled@Khaled-L.(none)>
2014-05-15 17:53:10 +0200

    Issue #450 Added method for adjusting timer's behaviour according to contest timings

--- tutor/app/assets/javascripts/contests.js
+++ tutor/app/assets/javascripts/contests.js
@@ -1,9 +1,9 @@
 jQuery(document).on("ready page:load", function() {
 	$(function () {
-		var name = $('#temp').data('name')
-		var date = contest.start_time
-		var start = new Date(date);
-		$('#countdown').countdown({until: start, onTick: highlightLast10});
+		var timer = $('#temp').data('timer')
+		var destroy = $('#temp').data('del') === 'true'
+		var count = new Date(timer);
+		$('#countdown').countdown(destroy ? 'destroy' : {until: count, onTick: highlightLast10});
 		function highlightLast10(periods) { 
 			if ($.countdown.periodsToSeconds(periods) === 10) { 
 				$(this).addClass('highlight'); 
--- tutor/app/controllers/contests_controller.rb
+++ tutor/app/controllers/contests_controller.rb
@@ -6,8 +6,22 @@ class ContestsController < ApplicationController
 
 		def validate_timer
 			@contest = Contest.find(params[:id])
-			@start = (@contest.start_date.to_s + " " + @contest.start_time.to_s).to_datetime
-			@end = (@contest.end_date.to_s + " " + @contest.end_time.to_s).to_datetime
+			contest_start = (@contest.start_date.to_s + " " + @contest.start_time.to_s).to_datetime
+			contest_end = (@contest.end_date.to_s + " " + @contest.end_time.to_s).to_datetime
+			current = DateTime.now
+			if current < contest_start
+				@timer = contest_start.to_s
+				@message = "Contest starts in"
+				@del = false
+			elsif current <= contest_end
+				@timer = contest_end.to_s
+				@message = "Contest ends in"
+				@del = false
+			else
+				@timer = DateTime.now
+				@message = "Contest has finished!"
+				@del = true
+			end	
 		end
 
 end
