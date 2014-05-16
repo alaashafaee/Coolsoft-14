32d9b834b921d88c6c3ac1a43dce72aedce39333
Mimi <mimi@ubuntu.(none)>
2014-05-16 18:09:13 +0200

    #414 fixing files

--- tutor/students_solutions/st1pr1so4/CoolSoft.java
+++
@@ -1,8 +0,0 @@
-public class CoolSoft {
-	public static void main(String [] args) {
-	    int x = Integer.parseInt(args[0);
-	    	    int y = Integer.parseInt(args[1);
-
-		System.out.println(x/y);
-	}
-}
