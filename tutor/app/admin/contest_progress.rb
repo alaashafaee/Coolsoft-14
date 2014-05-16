6f8d63954334b72c5ae2da6c70e8d074323b2320
Mimi <mimi@ubuntu.(none)>
2014-05-16 16:01:54 +0200

    #414 adjust identation

--- tutor/app/admin/contest_progress.rb
+++ tutor/app/admin/contest_progress.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register ContestProgress do
 
-  menu parent: "Contest", priority:2
-  permit_params :contest_id, :student_id, :cproblem_id, :status, :created_at, :updated_at
+	menu parent: "Contest", priority:2
+	permit_params	:contest_id, :student_id, :cproblem_id, :status, :created_at, :updated_at
 end
--- tutor/app/admin/track_progression.rb
+++ tutor/app/admin/track_progression.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register TrackProgression do
 
-  menu parent: "Subject", priority:5
-  permit_params :level, :student_id, :topic_id, :created_at, :updated_at
+	menu parent: "Subject", priority:5
+	permit_params	:level, :student_id, :topic_id, :created_at, :updated_at
 end
