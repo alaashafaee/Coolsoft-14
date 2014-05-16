bc858c2f1e61504773d6059cd86602db9cf3d599
Ahmed Atef <Ahmed@Ahmeds-MacBook-Pro-9.local>
2014-05-16 18:42:16 +0200

    #509 Added method notify lecturer

--- tutor/app/models/notification.rb
+++ tutor/app/models/notification.rb
@@ -7,6 +7,15 @@ class Notification < ActiveRecord::Base
 
 	#Scoops
 
-	#Methods
+	def self.lecturer_notify lecturer_id,course_id,student_id
+		lecturer = Lecturer.find_by_id(lecturer_id)
+		student = Student.find_by_id(student_id)
+		course = Course.find_by_id(course_id)
+		new_notification = Notification.new
+		new_notification.message = "#{student.name} subscribed to your course #{course.name}"
+		new_notification.save
+		lecturer.notifications << new_notification
+	end
+
 
 end
