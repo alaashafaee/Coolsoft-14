d9854966094c07995fdf605f5682c29162fb4d06
Mimi <mimi@ubuntu.(none)>
2014-05-16 18:11:25 +0200

    #414 fix identation

--- tutor/config/routes.rb
+++ tutor/config/routes.rb
@@ -1,7 +1,7 @@
 Tutor::Application.routes.draw do
 	
- 	ActiveAdmin.routes(self)
-    devise_for :admin_users, ActiveAdmin::Devise.config
+	ActiveAdmin.routes(self)
+	devise_for :admin_users, ActiveAdmin::Devise.config
 	devise_for :teaching_assistants
 	devise_for :students
 	devise_for :lecturers
@@ -21,7 +21,7 @@ Tutor::Application.routes.draw do
 	post 'courses/new' => 'courses#new'
 	post 'courses/share' => 'courses#share'
 	post 'solutions/execute' => 'solutions#execute'
- 	post '/posts/:id' => 'posts#update'
+	post '/posts/:id' => 'posts#update'
 	post 'tracks/insert_recommendation' => 'tracks#insert_recommendation'
 	post 'debuggers/:id' => 'debuggers#start'
 
@@ -46,7 +46,7 @@ Tutor::Application.routes.draw do
 	resources :courses
 	post "courses/choose"
 	post "courses/existing"
-  	post "courses/duplicate"
+	post "courses/duplicate"
 	get "model_answers/new"
 	post "model_answers/new"
 	resources :model_answers
