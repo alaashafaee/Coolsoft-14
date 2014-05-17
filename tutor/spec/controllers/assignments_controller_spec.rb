bcc760c0f4e2f1618dd1907cc1b0bbc59c0bc4ac
Nadine Adel <nadine@nadine-Inspiron-N4050.(none)>
2014-05-17 19:29:08 +0200

    Issue #421 added failure case in assignment test

--- tutor/spec/controllers/assignments_controller_spec.rb
+++ tutor/spec/controllers/assignments_controller_spec.rb
@@ -11,6 +11,10 @@ describe AssignmentsController do
 			confirmed_at: Time.now, dob: DateTime.now.to_date, gender: true,
 			degree: "PhD", university: "GUC", department: "MET")
 		@lecturer.save!
+		@course = Course.new(name:"SE2",
+			code:"Se2", year:2014, semester:6, :description => "last sprint",
+			:link => "http://met.guc.edu.eg/")
+		@course.save!
 	end
 
   	it "new returns http success" do
@@ -22,15 +26,19 @@ describe AssignmentsController do
 	context "CREATE" do
 		it "creates a new assignment" do
 			sign_in @lecturer
-	  		@course = Course.new(name:"SE",
-				code:"Se1", year:2014, semester:6, :description => "last sprint",
-				:link => "http://met.guc.edu.eg/")
-			@course.save!
 			expect{
 				get :create, assignment: {:title => "Assignmentse",
 				:description => "try to solve", "date(1i)" => "2014",
 				"date(2i)" => "12","date(3i)" => "12",},:course_id => @course.id
 			}.to change(Assignment,:count).by(1)
+		end
+		it 'is invalid assignment without title' do
+			sign_in @lecturer
+			expect{
+			get :create, assignment: {
+				:description => "try to solve1", "date(1i)" => "2014",
+				"date(2i)" => "12","date(3i)" => "12",},:course_id => @course.id
+			}.to change(Assignment,:count).by(0)
 		end
 	end
 end
