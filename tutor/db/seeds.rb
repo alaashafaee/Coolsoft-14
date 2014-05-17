13e6656ff442a1f20b244b2d64a329d09e8471bd
AbdullRahman ElHusseini <abdullrahman.elhusseni@robustastudio.com>
2014-05-17 19:49:08 +0200

    Issue #504 fixing seeds

--- tutor/db/seeds.rb
+++ tutor/db/seeds.rb
@@ -265,8 +265,8 @@ puts("# ----------------------- Assignments ----------------------- ")
 	Assignment.create(title:"CA Assignment", publish: false, description:"Allows practice on instruction set formats.", due_date: DateTime.now.to_date)
 
 puts("# ----------------------- Grades ----------------------- ")
-	Grade.create(grade: 100)
-	Grade.create(grade: 80)
+	Grade.create(grade: 100, student_id: 1, problem_id: 1)
+	Grade.create(grade: 80, student_id: 1, problem_id: 2)
 	
 
 puts("**************************************************************")
