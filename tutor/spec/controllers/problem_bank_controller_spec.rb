26524d757076a322294327216cdb46c63193b02d
Ahmed Sharaf <ahmed@ahmed-Aspire-E1-571G.(none)>
2014-05-17 22:38:30 +0300

    updated tests

--- tutor/spec/controllers/problem_bank_controller_spec.rb
+++ tutor/spec/controllers/problem_bank_controller_spec.rb
@@ -33,5 +33,43 @@ describe ProblemBankController do
 		get :show
 		expect(response).not_to render_template("show")
 	end
+	it 'Shows problem describtion successfully for lecturer/ta' do
+		problem = Problem.first
+		track_id = 1
+		lecturer = Lecturer.new(email: '1@lecturer.com', password: '123456789', 
+		password_confirmation: '123456789', name: 'LecturerI',
+		confirmed_at: Time.now, dob: DateTime.now.to_date, gender: true,
+		degree: "PhD", university: "GUC", department: "MET")
+		lecturer.save!
+		sign_in lecturer
+		get 'index' , {:id => problem.id, :track_id => track_id}
+		expect(response).to render_template("index")
+	end
+	it 'Does not Show problem describtion for student' do
+		problem = Problem.first
+		track_id = 1
+		student = Student.new(email: '1@studentt.com', password: '123456789', 
+		password_confirmation: '123456789', name: 'studentI',
+		confirmed_at: Time.now, dob: DateTime.now.to_date, 
+		gender: true, university: "GUC", faculty: "Engineering", semester: 6,major: "alwan")
+		student.save!
+		sign_in student
+		get 'index' , {:id => problem.id, :track_id => track_id}
+		expect(response).not_to render_template("index")
+	end
+	it 'Add a new problem from the bank to the lecturer/ta track' do
+		problem = Problem.find_by_id(2)
+		problemm = Problem.find_by_id(1)
+		track_id = 1
+		lecturer = Lecturer.new(email: '1@lecturer.com', password: '123456789', 
+		password_confirmation: '123456789', name: 'LecturerI',
+		confirmed_at: Time.now, dob: DateTime.now.to_date, gender: true,
+		degree: "PhD", university: "GUC", department: "MET")
+		lecturer.save!
+		sign_in lecturer
+		track = Track.find_by_id(track_id)
+		problemm.track_id = track_id
+		get 'add' , {:id => problem.id, :track_id => track_id}
+	end
 
 end
--- tutor/spec/models/problem_spec.rb
+++ tutor/spec/models/problem_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Problem do
-	it 'is vallid duplicate problem' do
+	it 'is valid duplicate problem' do
 		problem = Problem.first
 		duplicate_problem = problem.duplicate
 		expect(duplicate_problem).to be_valid
