4ec8b55d83d7131c6c344e27da2dd0c0ab28c86d
Rami Khalil <thesavagekoder@gmail.com>
2014-05-17 20:41:50 +0300

    Modified testing strategy. Updated rspec files. #570

--- tutor/spec/controllers/c_problems_controller_spec.rb
+++ tutor/spec/controllers/c_problems_controller_spec.rb
@@ -2,61 +2,9 @@ require 'spec_helper'
 
 describe CProblemsController do
 	include Devise::TestHelpers
-	render_views
-
-	let :my_lecturer do
-		lecturer = Lecturer.new(email: '1@lecturer.com', password: '123456789', 
-		password_confirmation: '123456789', name: 'LecturerI',
-		confirmed_at: Time.now, dob: DateTime.now.to_date, gender: true,
-		degree: "PhD", university: "GUC", department: "MET")
-		lecturer.save!
-		lecturer
-	end
-
-	let :bad_title do
-		cprob = Cproblem.new
-		cprob.title = ""
-		cprob.description = "I don't have a title"
-		cprob.time_limit = 42
-		cprob.owner_id = 1
-		cprob.owner_type = 'lecturer'
-		cprob
-	end
-
-	let :bad_description do
-		cprob = Cproblem.new
-		cprob.title = "I don't have a description"
-		cprob.description = ""
-		cprob.time_limit = 42
-		cprob.owner_id = 1
-		cprob.owner_type = 'lecturer'
-		cprob
-	end
-
-	let :bad_time_limit do
-		cprob = Cproblem.new
-		cprob.title = "What am I missing?"
-		cprob.description = "The proper time"
-		cprob.time_limit = ""
-		cprob.owner_id = 1
-		cprob.owner_type = 'lecturer'
-		cprob
-	end
-
-	let :good_problem do
-		cprob = Cproblem.new
-		cprob.title = "What's wrong with me?"
-		cprob.description = "Absolutely nothing."
-		cprob.time_limit = 123
-		cprob.owner_id = 1
-		cprob.owner_type = 'lecturer'
-		cprob
-	end
 
 	before(:each) do
-		@request.env["devise.mapping"] = Devise.mappings[:lecturer]
-		sign_in my_lecturer
-		expect(subject.current_lecturer).to_not be_nil
+		controller.class.skip_before_action :authenticate!
 	end
 
 	after(:each) do
@@ -72,29 +20,31 @@ describe CProblemsController do
 
 	describe "POST create" do
 		it "creates a new contest problem" do
-			good_problem
-			post :create, cproblem: good_problem
-
-			render
-			expect(rendered).to contain("Your contest problem has been created.")
+			expect {
+				post :create, cproblem: {title: "Test", description: "Test", time_limit: 20}
+			}.to change(Cproblem, :count)
+			expect(flash[:notice]).to_not be_nil
 		end
 
 		it "rejects blank problem titles" do
-			bad_title
-			post :create, cproblem: bad_title
-			expect(response.body).to match /blank title./m
+			expect {
+				post :create, cproblem: {title: "", description: "Test", time_limit: 20}
+			}.to_not change(Cproblem, :count)
+			expect(flash[:notice]).to_not be_nil
 		end
 
 		it "rejects blank problem descriptions" do
-			bad_description
-			post :create, cproblem: bad_description
-			expect(response.body).to match /blank description./m
+			expect {
+				post :create, cproblem: {title: "Test", description: "", time_limit: 20}
+			}.to_not change(Cproblem, :count)
+			expect(flash[:notice]).to_not be_nil
 		end
 
 		it "rejects invalid problem time limits" do
-			bad_time_limit
-			post :create, cproblem: bad_time_limit
-			expect(response.body).to match /time limit must be a numeric value greater than zero./m
+			expect {
+				post :create, cproblem: {title: "Test", description: "Test", time_limit: "lol"}
+			}.to_not change(Cproblem, :count)
+			expect(flash[:notice]).to_not be_nil
 		end
 
 	end
