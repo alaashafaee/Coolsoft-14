526c702c4a17ac2ee7082ce40ade8c2c2e562f8f
Nadine Adel <nadine@nadine-Inspiron-N4050.(none)>
2014-05-17 14:53:55 +0200

    Issue #421 Remove unneeded files

--- tutor/spec/controllers/assignments_controller_spec.rb
+++ tutor/spec/controllers/assignments_controller_spec.rb
@@ -10,20 +10,24 @@ describe AssignmentsController do
   before(:each) do
     sign_out :user
   end
-describe "#new" do
- 
-end
-describe "POST #create" do
 
  it "saves the new tag in the database" do
-        expect{
-          post :create, assignment: {:title => 'Software',:description =>'dssd'}
-        }.to change(Assignment,:count).by(1)
-      end
-       before(:each) do
-    sign_out :user
-  end
+  lecturer = Lecturer.create(name: "sameh", gender: true, degree: "PhD",
+          department: "MET", university: "GUC", email: "1@tas.com",
+          encrypted_password: "string",
+          reset_password_token: "string",
+          confirmation_token: "true")
+          course = Course.create(name:"Data Structures and Alogrithms", 
+          description:"This is a very easy course", code:"CSEN1", year:2014, semester:1)
   
+       AssignmentController.skip_before_filter :authenticate!
+    
+        expect{
+          get :create, assignment=> {title:'Software',description:'dssd'},:course_id=>course.id
+        }.to change(Assignment,:count).by(+1)
+      end
+
+
     it "takes three parameters and returns a Book object" do
         @new_assignment.should be_an_instance_of Assignment
     end
