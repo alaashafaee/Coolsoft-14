1d3cdb96f64d04a157398dd693cdfbce944b05fc
Muhammad Mamdouh <mamdou7@mamdou7-Satellite-L500.(none)>
2014-05-18 01:16:00 +0200

    Issue #526 fixing conflicts

--- tutor/spec/controllers/contests_controller_spec.rb
+++ tutor/spec/controllers/contests_controller_spec.rb
@@ -33,6 +33,24 @@ describe ContestsController do
 			get :index
 			expect(response).to render_template("index")
 		end
+	end	
+
+	describe "GET show" do
+		before(:each) do
+			@contest1 = Contest.create(title: 'TestContest', description: 'Some description')
+		end
+
+		it "assigns the requested contest to the @contest" do
+			ContestsController.skip_before_filter :authenticate!
+			get :show, id: @contest1.id
+			expect(assigns(:contest)).to eq(@contest1)
+		end
+
+		it "render show template" do
+			ContestsController.skip_before_filter :authenticate!	
+			get :show, id: @contest1.id
+			expect(response).to render_template("show")
+		end
 	end
 
 	context "CRUD" do
