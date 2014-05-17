1ea1af4db07ab45be80111b17764dd0a3df65b56
AmirGeorge <dangerousmiro@gmail.com>
2014-05-18 01:15:26 +0200

    #447 indendation fixes

--- tutor/spec/controllers/contests_controller_spec.rb
+++ tutor/spec/controllers/contests_controller_spec.rb
@@ -14,11 +14,13 @@ describe ContestsController do
 				confirmed_at: Time.now, dob: DateTime.now.to_date, gender: true,
 				degree: "PhD", university: "GUC", department: "MET")
 			@lecturer.save!
-			course = Course.create(name:"DSA", description:"good",
-				code:"CSEN1", link:"http://www.test.org/", year:2014, semester:1)
+			course = Course.create(name: "DSA", description: "good",
+				code: "CSEN1", link: "http://www.test.org/", year: 2014,
+				semester: 1)
 			course.save!
-			@contest = Contest.new(title:"Iteration2", description:"If you",
-				incomplete:false, start_time:Time.now+2.days, end_time:Time.now+7.days)
+			@contest = Contest.new(title: "Iteration2", description: "If you",
+				incomplete: false, start_time: Time.now+2.days,
+				end_time: Time.now+7.days)
 			@contest.save!
 			course.contests << @contest
 			@lecturer.contests << @contest
@@ -61,18 +63,18 @@ describe ContestsController do
 		it "creates a new contest" do
 			sign_in @lecturer
 			course = Course.new(name: "Amirooo",
-				code: "tip101", year: 2014, semester: 1, :description => "hary",
-				:link => "http://www.test.org/")
+				code: "tip101", year: 2014, semester: 1,
+				:description => "hary", :link => "http://www.test.org/")
 			course.save!
 			@lecturer.courses << course
 			expect{
-				get :create, contest: {:title => "Amir contest", :course => "Amirooo",
-					:description => "hardest", "start_time(1i)" => "2014",
-					"start_time(2i)" => "5","start_time(3i)" => "19",
-					"start_time(4i)" => "22","start_time(5i)" => "01",
-					"end_time(1i)" => "2014","end_time(2i)" => "9",
-					"end_time(3i)" => "16","end_time(4i)" => "22",
-					"end_time(5i)" => "01",}
+				get :create, contest: {:title => "Amir contest",
+					:course => "Amirooo", :description => "hardest",
+					"start_time(1i)" => "2014", "start_time(2i)" => "5",
+					"start_time(3i)" => "19", "start_time(4i)" => "22",
+					"start_time(5i)" => "01", "end_time(1i)" => "2014",
+					"end_time(2i)" => "9", "end_time(3i)" => "16",
+					"end_time(4i)" => "22", "end_time(5i)" => "01",}
 			}.to change(Contest,:count).by(1)
 		end
 	end
@@ -80,10 +82,10 @@ describe ContestsController do
 	context "Timer" do
 
 		before(:all) do
-			Contest.create(title:"Iteration", description:"If you solve this",
-				incomplete:false, start_time:DateTime.now+1.minute,
-				end_time:DateTime.now+10.minutes, course_id:1,
-				owner_id:1, owner_type:"Lecturer")
+			Contest.create(title: "Iteration", description: "If you solv",
+				incomplete: false, start_time: DateTime.now + 1.minute,
+				end_time: DateTime.now + 10.minutes, course_id:1,
+				owner_id: 1, owner_type: "Lecturer")
 			@c = Contest.first
 		end
 
