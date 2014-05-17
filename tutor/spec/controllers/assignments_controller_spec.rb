fe4a6036f7d031ac43ddfa2eea3eef8a0607bba4
Nadine Adel <nadine@nadine-Inspiron-N4050.(none)>
2014-05-17 14:27:47 +0200

    Issue #421 removing unneeded files

--- tutor/spec/controllers/assignments_controller_spec.rb
+++ tutor/spec/controllers/assignments_controller_spec.rb
@@ -1,39 +1,35 @@
 require 'spec_helper'
+include Devise::TestHelpers
+class ActiveSupport::TestCase
+include Devise::TestHelpers
+end
 
 describe AssignmentsController do
-  include Devise::TestHelpers
 
 
+  before(:each) do
+    sign_out :user
+  end
 describe "#new" do
+ 
+end
+describe "POST #create" do
+
+ it "saves the new tag in the database" do
+        expect{
+          post :create, assignment: {:title => 'Software',:description =>'dssd'}
+        }.to change(Assignment,:count).by(1)
+      end
+       before(:each) do
+    sign_out :user
+  end
+  
     it "takes three parameters and returns a Book object" do
         @new_assignment.should be_an_instance_of Assignment
     end
-end
-describe "GET #create" do
-    context "with valid attributes" do
-      it "saves the new tag in the database" do
-        expect{
-          get :create, assignment: {:title => 'Software',:description =>'dssd'}
-        }.to change(Assignment,:count).by(1)
-      end
     end
   
 
-  end
-    context "with valid attributes " do
-    it "creates a new comment" do
-      i = Course.new
-      i.name="Data Structures and Alogrithms"
-       i.description="This is a very easy course" 
-       i.code="CSEN1"
-       i.year=2014
-        i.semester=1
-        i.save
-
-      expect{
-        post :create, :course_id => i.id
-      }.to change(Assignment,:count).by(1)
-    end
-  end
+    
 
 end
