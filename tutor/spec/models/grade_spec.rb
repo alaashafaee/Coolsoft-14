e2343eb1c5a8f622fdf3bc8e3b301de6770f7029
AbdullRahman ElHusseini <abdullrahman.elhusseni@robustastudio.com>
2014-05-18 00:12:45 +0200

    Issue #504 fixing specs in grade model

--- tutor/spec/models/grade_spec.rb
+++ tutor/spec/models/grade_spec.rb
@@ -1,36 +1,24 @@
 require 'spec_helper'
 
-describe Note do
+describe Grade do
 	it 'is valid with a grade, student_id and problem_id' do
-		student = Student.new(email: '1@student.com', password: '123456789',
-		password_confirmation: '123456789', name: 'StudentI',
-		confirmed_at: Time.now, dob: DateTime.now.to_date, gender: true,
-		faculty: "MET", university: "GUC", major: "CS", semester: 6,
-		advising: false, probation: false)
-		student.save
 		problem = AssignmentProblem.new(title: "AssignmentProblem 1",
 			description: "long description", final_grade: 100)
 		problem.save
 		grade = Grade.new(
 			grade: 10,
-			student_id: student.id,
+			student_id: 1,
 			problem_id: problem.id)
 		grade.save
 		expect(grade).to be_valid
 	end
 
 	it 'is invalid without a grade' do
-		student = Student.new(email: '1@student.com', password: '123456789',
-		password_confirmation: '123456789', name: 'StudentI',
-		confirmed_at: Time.now, dob: DateTime.now.to_date, gender: true,
-		faculty: "MET", university: "GUC", major: "CS", semester: 6,
-		advising: false, probation: false)
-		student.save
 		problem = AssignmentProblem.new(title: "AssignmentProblem 1",
 			description: "long description", final_grade: 100)
 		problem.save
 		grade = Grade.new(
-			student_id: student.id,
+			student_id: 1,
 			problem_id: problem.id)
 		grade.save
 		expect(grade).not_to be_valid
@@ -48,32 +36,20 @@ describe Note do
 	end
 
 	it 'is invalid without problem_id' do
-		student = Student.new(email: '1@student.com', password: '123456789',
-		password_confirmation: '123456789', name: 'StudentI',
-		confirmed_at: Time.now, dob: DateTime.now.to_date, gender: true,
-		faculty: "MET", university: "GUC", major: "CS", semester: 6,
-		advising: false, probation: false)
-		student.save
 		grade = Grade.new(
 			grade: 10,
-			student_id: student.id)
+			student_id: 1)
 		grade.save
 		expect(grade).not_to be_valid
 	end
 
 	it 'Changes the number of grades' do
-		student = Student.new(email: '1@student.com', password: '123456789',
-		password_confirmation: '123456789', name: 'StudentI',
-		confirmed_at: Time.now, dob: DateTime.now.to_date, gender: true,
-		faculty: "MET", university: "GUC", major: "CS", semester: 6,
-		advising: false, probation: false)
-		student.save
 		problem = AssignmentProblem.new(title: "AssignmentProblem 1",
 			description: "long description", final_grade: 100)
 		problem.save
 		grade = Grade.new(
 			grade: 10,
-			student_id: student.id,
+			student_id: 1,
 			problem_id: problem.id)
 		expect {grade.save}.to change {Grade.count}.by(1)
 	end
