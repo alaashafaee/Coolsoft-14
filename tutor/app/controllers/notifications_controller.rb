c0ececff3ef52323dd8431077e3ff1d375ddda7e
Muhammad Mamdouh <mamdou7@mamdou7-Satellite-L500.(none)>
2014-05-18 00:43:22 +0200

    Issue #516 removing Atef's index

--- tutor/app/controllers/notifications_controller.rb
+++ tutor/app/controllers/notifications_controller.rb
@@ -32,21 +32,4 @@ class NotificationsController < ApplicationController
 		end	
 	end
 
-	# [Notification sending-story 2.9]
-	# Description: Displays Notifications of the user
-	# Parameters:
-	#	@notifications: The current notifications of the current user
-	# Returns: The view of the notifications
-	# Author: Ahmed Atef
-	def index
-		if lecturer_signed_in?
-			@notifications = Lecturer.find(params[:id]).notifications.order("created_at desc")
-		elsif teaching_assistant_signed_in?
-			@notifications = TeachingAssistant.find(params[:id])
-				.notifications.order("created_at desc")
-		else
-			@notifications = Student.find(params[:id]).notifications.order("created_at desc")
-		end
-	end 
-
 end
