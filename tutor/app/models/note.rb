ae090294b098126af1d86d451cd500d08bcbf0fe
Ahmed Atef <Ahmed@Ahmeds-MacBook-Pro-9.local>
2014-05-17 11:10:04 +0200

    Merged master with my branch

--- tutor/app/models/note.rb
+++ tutor/app/models/note.rb
@@ -1,7 +1,9 @@
 class Note < ActiveRecord::Base
 
 	#Validations
-
+	validates_presence_of :content
+	validates_presence_of :solution_id
+	validates_presence_of :line
 	#Relations
 	belongs_to :owner, polymorphic: true
 	belongs_to :problems
