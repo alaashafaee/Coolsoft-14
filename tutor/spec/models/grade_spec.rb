add6af97ce4f81c10f34ddac69334648cd95cbcd
AbdullRahman ElHusseini <abdullrahman.elhusseni@robustastudio.com>
2014-05-17 21:04:52 +0200

    Issue #504 adding specs in controller and fixing specs in model

--- tutor/spec/models/grade_spec.rb
+++ tutor/spec/models/grade_spec.rb
@@ -2,43 +2,79 @@ require 'spec_helper'
 
 describe Note do
 	it 'is valid with a grade, student_id and problem_id' do
+		student = Student.new(email: '1@student.com', password: '123456789',
+		password_confirmation: '123456789', name: 'StudentI',
+		confirmed_at: Time.now, dob: DateTime.now.to_date, gender: true,
+		faculty: "MET", university: "GUC", major: "CS", semester: 6,
+		advising: false, probation: false)
+		student.save
+		problem = AssignmentProblem.new(title: "AssignmentProblem 1",
+			description: "long description", final_grade: 100)
+		problem.save
 		grade = Grade.new(
 			grade: 10,
-			student_id: 1,
-			problem_id: 1)
+			student_id: student.id,
+			problem_id: problem.id)
 		grade.save
 		expect(grade).to be_valid
 	end
 
 	it 'is invalid without a grade' do
+		student = Student.new(email: '1@student.com', password: '123456789',
+		password_confirmation: '123456789', name: 'StudentI',
+		confirmed_at: Time.now, dob: DateTime.now.to_date, gender: true,
+		faculty: "MET", university: "GUC", major: "CS", semester: 6,
+		advising: false, probation: false)
+		student.save
+		problem = AssignmentProblem.new(title: "AssignmentProblem 1",
+			description: "long description", final_grade: 100)
+		problem.save
 		grade = Grade.new(
-			student_id: 1,
-			problem_id: 1)
+			student_id: student.id,
+			problem_id: problem.id)
 		grade.save
 		expect(grade).not_to be_valid
 	end
 
 	it 'is invalid without student_id' do
+		problem = AssignmentProblem.new(title: "AssignmentProblem 1",
+			description: "long description", final_grade: 100)
+		problem.save
 		grade = Grade.new(
 			grade: 10,
-			problem_id: 1)
+			problem_id: problem.id)
 		grade.save
 		expect(grade).not_to be_valid
 	end
 
 	it 'is invalid without problem_id' do
+		student = Student.new(email: '1@student.com', password: '123456789',
+		password_confirmation: '123456789', name: 'StudentI',
+		confirmed_at: Time.now, dob: DateTime.now.to_date, gender: true,
+		faculty: "MET", university: "GUC", major: "CS", semester: 6,
+		advising: false, probation: false)
+		student.save
 		grade = Grade.new(
 			grade: 10,
-			student_id: 1)
+			student_id: student.id)
 		grade.save
 		expect(grade).not_to be_valid
 	end
 
 	it 'Changes the number of grades' do
+		student = Student.new(email: '1@student.com', password: '123456789',
+		password_confirmation: '123456789', name: 'StudentI',
+		confirmed_at: Time.now, dob: DateTime.now.to_date, gender: true,
+		faculty: "MET", university: "GUC", major: "CS", semester: 6,
+		advising: false, probation: false)
+		student.save
+		problem = AssignmentProblem.new(title: "AssignmentProblem 1",
+			description: "long description", final_grade: 100)
+		problem.save
 		grade = Grade.new(
 			grade: 10,
-			student_id: 1,
-			problem_id: 1)
+			student_id: student.id,
+			problem_id: problem.id)
 		expect {grade.save}.to change {Grade.count}.by(1)
 	end
 end
