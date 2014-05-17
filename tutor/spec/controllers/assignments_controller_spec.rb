59a249935766df7220d15c82f36449d9639cb18a
Nadine Adel <nadine@nadine-Inspiron-N4050.(none)>
2014-05-17 10:45:01 +0200

    Issue #421 add information

--- tutor/spec/controllers/assignments_controller_spec.rb
+++ tutor/spec/controllers/assignments_controller_spec.rb
@@ -1,56 +1,39 @@
 require 'spec_helper'
-include Devise::TestHelpers
+
 describe AssignmentsController do
-  before :each do
-    @new_assignment = Assignment.new "Title", "Author"
-end
+  include Devise::TestHelpers
+
 
 describe "#new" do
     it "takes three parameters and returns a Book object" do
         @new_assignment.should be_an_instance_of Assignment
     end
 end
-describe "POST 'create'" do
-    it 'should assign top videos' do
-      assignment_params = { :title => 'new assignment', :description => 'assignment description' }
-      get :create, :new_assignment => assignment_params
-      assigns[:new_assignment].should_not be_new_record
-      assigns[:new_assignment].title.should == params[:title]
-      assigns[:new_assignment].description.should == params[:description]
+describe "GET #create" do
+    context "with valid attributes" do
+      it "saves the new tag in the database" do
+        expect{
+          get :create, assignment: {:title => 'Software',:description =>'dssd'}
+        }.to change(Assignment,:count).by(1)
+      end
     end
-  end
-
-  describe "POST 'create'" do
-
-    before(:each) do
-      @user = FactoryGirl.create(:user)
-
-      @article = FactoryGirl.build(:article)
-      @article.user_id = @user.id
-      @article.save
-
-      @article_attributes = FactoryGirl.attributes_for(:article)
-      @comment_attributes = FactoryGirl.attributes_for(:comment)
-    end
-
-    it "should create a new comment" do
-      expect {
-        post :create, :comment => @comment_attributes
-      }.to change(Comment, :count).by(1)
-    end
-
-    it "should create a new comment, redirect to the article show page of this comment and notify the user on successful saving of the comment" do
-      post :create, :comment => @comment_attributes, :article_id => @article.id.to_s, :user_id => @user.id.to_s
-      flash[:notice].should_not be_nil
-      response.should redirect_to(article_path(@article))
-    end
-
-  end
-
-
- 
-
-
   
+
+  end
+    context "with valid attributes " do
+    it "creates a new comment" do
+      i = Course.new
+      i.name="Data Structures and Alogrithms"
+       i.description="This is a very easy course" 
+       i.code="CSEN1"
+       i.year=2014
+        i.semester=1
+        i.save
+
+      expect{
+        post :create, :course_id => i.id
+      }.to change(Assignment,:count).by(1)
+    end
+  end
 
 end
