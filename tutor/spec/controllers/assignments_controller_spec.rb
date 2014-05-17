236aff2413764e82564dba5ab84b9710de015184
Nadine Adel <nadine@nadine-Inspiron-N4050.(none)>
2014-05-18 01:22:51 +0200

    Issue #422 add test for edit assignment

--- tutor/spec/controllers/assignments_controller_spec.rb
+++ tutor/spec/controllers/assignments_controller_spec.rb
@@ -6,11 +6,6 @@ end
 
 describe AssignmentsController do
 	before (:all) do
-		@lecturer = Lecturer.new(email: '1@lecturer.com', password: '123456789',
-			password_confirmation: '123456789', name: 'LecturerI',
-			confirmed_at: Time.now, dob: DateTime.now.to_date, gender: true,
-			degree: "PhD", university: "GUC", department: "MET")
-		@lecturer.save!
 		@course = Course.new(name:"SE2",
 			code:"Se2", year:2014, semester:6, :description => "last sprint",
 			:link => "http://met.guc.edu.eg/")
@@ -26,6 +21,29 @@ describe AssignmentsController do
 			expect {
 				delete :destroy, :id => assignment.id
 			}.to change(Assignment, :count).by(-1)
+		end
+	end
+	context "edit" do
+		it "edit the requested assignment" do
+			AssignmentsController.skip_before_filter :authenticate!
+			@assignment = Assignment.new(title: "assignmentdelete1",
+				description: "will be deleted", course_id: @course.id)
+			@assignment.save!
+			expect{
+				put :update, assignment: {
+					:description => "try to solve1"},:id => @assignment.id
+				@assignment.reload
+			}.to change{@assignment.description}.from("will be deleted").to("try to solve1") 
+		end
+		it "edit the requested assignment invalid as title can not be empty" do
+			AssignmentsController.skip_before_filter :authenticate!
+			@assignment = Assignment.new(title: "assignmentdelete2",
+				description: "will be deleted", course_id: @course.id)
+			@assignment.save!
+			put :update, assignment: {
+				:title => ""},:id => @assignment.id
+				@assignment.reload
+			@assignment.title.should eq("assignmentdelete2")
 		end
 	end
 end
