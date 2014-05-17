0e79441f26a97fb6f4894161ee4acff560dd7543
MussabElDash <mussab14@gmail.com>
2014-05-17 13:19:45 +0300

    #519 Fix Fix some specs

--- tutor/app/models/python_compiler.rb
+++ tutor/app/models/python_compiler.rb
@@ -31,7 +31,7 @@ class PythonCompiler
 	# Author: Ahmed Moataz
 	def self.compiler_feedback(solution)
 		feedback = compile(solution, solution.code)
-		feedback = feedback.gsub("\u0000"," ")
+		feedback = feedback.gsub("\u0000"," ").gsub("\u001A"," ")
 		if feedback == ""
 			return {success: true, errors: nil}
 		else
