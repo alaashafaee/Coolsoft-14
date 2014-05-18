d848ba7178f58bf24c2a420195d461b6478a700f
metawaa <metawaa@Metawaa.(none)>
2014-05-18 07:08:24 +0200

    #485 adding a new test

--- tutor/spec/controllers/courses_controller_spec.rb
+++ tutor/spec/controllers/courses_controller_spec.rb
@@ -5,8 +5,8 @@ describe CoursesController do
 	describe "POST hide" do
 		it "Seen value changes after load" do
 			lecturer = Lecturer.new(email: '1@lecturer.com', password: '123456789', 
- 			password_confirmation: '123456789', name: 'LecturerI',
- 			confirmed_at: Time.now, dob: DateTime.now.to_date, gender: true,
+			password_confirmation: '123456789', name: 'LecturerI',
+			confirmed_at: Time.now, dob: DateTime.now.to_date, gender: true,
 			degree: "PhD", university: "GUC", department: "MET")
 			lecturer.save!
 			course = 	Course.new(name:"Data Structures and Alogrithms", 
@@ -17,6 +17,21 @@ describe CoursesController do
 			sign_in lecturer
 			post :hide, :id => lecturer.courses.first.id
 			expect(lecturer.courses.first.visible).to be(true)
+		end
+		it "Seen value changes" do
+			lecturer = Lecturer.new(email: '1@lecturer.com', password: '123456789', 
+			password_confirmation: '123456789', name: 'LecturerI',
+			confirmed_at: Time.now, dob: DateTime.now.to_date, gender: true,
+			degree: "PhD", university: "GUC", department: "MET")
+			lecturer.save!
+			course = 	Course.new(name:"Data Structures and Alogrithms", 
+				description:"This is a very easy course",
+				code:"CSEN11", year:2014, semester:1 ,visible: true)
+			course.save
+			lecturer.courses << course
+			sign_in lecturer
+			post :hide, :id => lecturer.courses.first.id
+			expect(lecturer.courses.first.visible).to be(false)
 		end
 	end
 end
